0333455a3e52e4bdfb5299e7d7e0219c3e773e4b
Jack Maloney <jack.maloney@digital.justice.gov.uk>
2022-04-08 14:41:14 +0100

    Update UI casing

--- server/utils/nunjucksSetup.ts
+++ server/utils/nunjucksSetup.ts
@@ -9,7 +9,7 @@ export default function nunjucksSetup(app: express.Express, path: pathModule.Pla
   app.set('view engine', 'njk')
 
   app.locals.asset_path = '/assets/'
-  app.locals.applicationName = 'Make Recall Decision Ui'
+  app.locals.applicationName = 'Make Recall Decision UI'
 
   // Cachebusting version string
   if (production) {
