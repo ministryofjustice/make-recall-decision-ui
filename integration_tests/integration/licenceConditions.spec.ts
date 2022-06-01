4b0d3623d57cb2676afa62c5c6d2f6a522ab77c9
Jon Wyatt <>
2022-06-01 16:55:12 +0100

    Hide data points for second licence

--- integration_tests/integration/licenceConditions.spec.ts
+++ integration_tests/integration/licenceConditions.spec.ts
@@ -12,10 +12,16 @@ context('Licence conditions', () => {
     const crn = 'X34983'
     cy.visit(`${routeUrls.cases}/${crn}/licence-conditions`)
     cy.pageHeading().should('equal', 'Licence conditions')
+    cy.getElement('Release from custody date', { parent: `[data-qa="summary-1"]` }).should('exist')
+    cy.getElement('Licence expiry date', { parent: `[data-qa="summary-1"]` }).should('exist')
+    cy.getElement('Post-sentence supervision end date', { parent: `[data-qa="summary-1"]` }).should('exist')
+    cy.getElement('Release from custody date', { parent: `[data-qa="summary-2"]` }).should('not.exist')
+    cy.getElement('Licence expiry date', { parent: `[data-qa="summary-2"]` }).should('not.exist')
+    cy.getElement('Post-sentence supervision end date', { parent: `[data-qa="summary-2"]` }).should('not.exist')
     getCaseLicenceConditionsResponse.offences.forEach((offence, offenceIndex) => {
-      cy.getDefinitionListValue('Offence').should(
+      cy.getDefinitionListValue('Offence', { parent: `[data-qa="summary-${offenceIndex + 1}"]` }).should(
         'contain',
-        '(Criminal damage endangering life (excluding arson) - aircraft - 05713)'
+        offence.offences[0].description
       )
       offence.licenceConditions.forEach((condition, conditionIndex) => {
         cy.getRowValuesFromTable({
