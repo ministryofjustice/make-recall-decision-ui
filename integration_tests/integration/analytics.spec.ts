c9f0b010bf8606e9561792c5676f7b6117f8b665
Jon Wyatt <>
2022-06-07 09:45:38 +0100

    Stub GA post request

--- integration_tests/integration/analytics.spec.ts
+++ integration_tests/integration/analytics.spec.ts
@@ -9,6 +9,7 @@ context('Analytics', () => {
   it('sends a page view event to Google Analytics', () => {
     const crn = 'X34983'
     cy.intercept('GET', 'https://www.google-analytics.com/collect?*', { statusCode: 200 }).as('collect')
+    cy.intercept('POST', 'https://www.google-analytics.com/j/collect', { statusCode: 200 })
     cy.visit(`${routeUrls.cases}/${crn}/overview`)
     cy.wait('@collect')
       .then(data => data.request.url)
