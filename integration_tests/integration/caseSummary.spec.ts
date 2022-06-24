4aca0ce8c8d32836a8173f6c46b0d70b6762d565
Jon Wyatt <>
2022-06-24 13:49:34 +0100

    Risk - show RoSH and MAPPA if data available

--- integration_tests/integration/caseSummary.spec.ts
+++ integration_tests/integration/caseSummary.spec.ts
@@ -1,5 +1,4 @@
 import getCaseOverviewResponse from '../../api/responses/get-case-overview.json'
-import getCaseRiskResponse from '../../api/responses/get-case-risk.json'
 import { routeUrls } from '../../server/routes/routeUrls'
 import { formatDateTimeFromIsoString } from '../../server/utils/dates/format'
 
@@ -69,54 +68,6 @@ context('Case summary', () => {
     cy.getLinkHref('jenny@probation.com').should('equal', 'mailto:jenny@probation.com')
   })
 
-  it('can view the risk page', () => {
-    const crn = 'X34983'
-    cy.visit(`${routeUrls.cases}/${crn}/risk`)
-    cy.pageHeading().should('equal', 'Risk for Paula Smith')
-
-    // Content panels
-    cy.viewDetails('View more detail on Details of the risk').should(
-      'contain',
-      getCaseRiskResponse.natureOfRisk.description
-    )
-    cy.viewDetails('View more detail on Who is at risk').should('contain', getCaseRiskResponse.whoIsAtRisk.description)
-    cy.viewDetails('View more detail on Circumstances that will increase the risk').should(
-      'contain',
-      getCaseRiskResponse.circumstancesIncreaseRisk.description
-    )
-
-    // RoSH table
-    cy.getRowValuesFromTable({ tableCaption: 'Risk of serious harm', firstColValue: 'Children' }).then(rowValues => {
-      expect(rowValues[0]).to.equal('Low')
-    })
-    cy.getRowValuesFromTable({ tableCaption: 'Risk of serious harm', firstColValue: 'Public' }).then(rowValues => {
-      expect(rowValues[0]).to.equal('Very high')
-    })
-    cy.getRowValuesFromTable({ tableCaption: 'Risk of serious harm', firstColValue: 'Known adult' }).then(rowValues => {
-      expect(rowValues[0]).to.equal('Medium')
-    })
-    cy.getRowValuesFromTable({ tableCaption: 'Risk of serious harm', firstColValue: 'Staff' }).then(rowValues => {
-      expect(rowValues[0]).to.equal('High')
-    })
-
-    // MAPPA level
-    cy.getElement('CAT 2/LEVEL 1').should('exist')
-
-    // predictor graphs
-    cy.getElement('Risk of serious recidivism (RSR) score - 23').should('exist')
-    cy.getElement('OSP/C score').should('exist')
-    cy.getElement('OSP/I score').should('exist')
-    cy.getElement('OGRS score - 12').should('exist')
-
-    // score history
-    cy.getElement('14 May 2019 at 12:00').should('exist')
-    cy.getElement('12 September 2018 at 12:00').should('exist')
-    cy.getElement('RSR HIGH 18').should('not.be.visible')
-    cy.clickLink('Open all')
-    cy.getElement('RSR HIGH 18').should('be.visible')
-    cy.getElement('RSR MEDIUM 12').should('be.visible')
-  })
-
   it('can switch between case summary pages', () => {
     cy.task('getCase', { sectionId: 'overview', statusCode: 200, response: getCaseOverviewResponse })
     const crn = 'X34983'
@@ -130,13 +81,5 @@ context('Case summary', () => {
     cy.pageHeading().should('equal', 'Licence conditions for Charles Edwin')
     cy.clickLink('Overview')
     cy.pageHeading().should('equal', 'Overview for Paula Smith')
-  })
-
-  it('can switch to risk page if flag is enabled', () => {
-    cy.task('getCase', { sectionId: 'overview', statusCode: 200, response: getCaseOverviewResponse })
-    const crn = 'X34983'
-    cy.visit(`${routeUrls.cases}/${crn}/overview?flagShowMockedUi=1`)
-    cy.clickLink('Risk')
-    cy.pageHeading().should('equal', 'Risk for Paula Smith')
   })
 })
