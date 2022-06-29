93eee5a39ca46c00a4c537b61160909849bd02e1
Jon Wyatt <jonnywyatt2@gmail.com>
2022-06-29 17:00:57 +0100

    MRD-290 - updates to task list, evidence, page order (#156)
    Co-authored-by: Jon Wyatt <>

--- server/controllers/recommendation/refData/alternativesToRecallRefData.ts
+++ server/controllers/recommendation/refData/alternativesToRecallRefData.ts
@@ -9,4 +9,34 @@ export const alternativesToRecallRefData = [
     value: 'REPORTING_FREQUENCY',
     detailsLabel: 'Give details',
   },
+  {
+    text: 'Extra licence conditions',
+    value: 'EXTRA_LICENCE_CONDITIONS',
+    detailsLabel: 'Give details',
+  },
+  {
+    text: 'Referral to other teams (for example, IOM, MAPPA, Gangs Unit)',
+    value: 'REFERRAL_TO_TEAMS',
+    detailsLabel: 'Give details',
+  },
+  {
+    text: 'Referral to partnership agencies',
+    value: 'REFERRAL_TO_AGENCIES',
+    detailsLabel: 'Give details',
+  },
+  {
+    text: 'Referral to approved premises',
+    value: 'REFERRAL_TO_PREMISES',
+    detailsLabel: 'Give details',
+  },
+  {
+    text: 'Drug testing',
+    value: 'DRUG_TESTING',
+    detailsLabel: 'Give details',
+  },
+  {
+    text: 'Other',
+    value: 'OTHER',
+    detailsLabel: 'Give details',
+  },
 ]
