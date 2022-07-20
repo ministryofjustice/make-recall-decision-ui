99b3d3eeccaf416fff5eae1cd2539bd50d4fc84a
Jon Wyatt <>
2022-07-20 15:41:01 +0100

    View details events

--- integration_tests/integration/analytics.spec.ts
+++ integration_tests/integration/analytics.spec.ts
@@ -5,15 +5,34 @@ context('Analytics', () => {
     cy.signIn()
   })
 
-  it('sends page view and form error events to Google Analytics', () => {
+  it('sends page view, form error and view details events to Google Analytics', () => {
+    const crn = 'X34983'
     cy.interceptGoogleAnalyticsEvent()
-    cy.visit(`${routeUrls.searchResults}?crn=`)
+    cy.visit(`${routeUrls.cases}/${crn}/contact-history`)
     cy.wait('@googleAnalyticsPageView')
       .then(data => data.request.url)
       .should('contain', 't=pageview')
-    // form validation error ('missingCrn')
+    // form validation error
+    cy.fillInput('Day', '12', { parent: '#dateFrom' })
+    cy.fillInput('Month', '04', { parent: '#dateFrom' })
+    cy.fillInput('Day', '13', { parent: '#dateTo' })
+    cy.fillInput('Month', '05', { parent: '#dateTo' })
+    cy.fillInput('Year', '2022', { parent: '#dateTo' })
+    cy.clickButton('Apply filters')
     cy.wait('@googleAnalyticsEvent')
       .then(data => data.request.url)
-      .should('contain', 'ec=missingCrn&ea=form_error&el=crn')
+      .should('contain', 'ec=missingDateParts&ea=form_error&el=dateFrom')
+    // open / close contact details
+    cy.viewDetails('View more detail', { parent: `[data-qa="contact-1"]` }).should(
+      'contain',
+      'Comment added by Eliot Prufrock on 20/04/2022 at 11:35'
+    )
+    cy.wait('@googleAnalyticsEvent')
+      .then(data => data.request.url)
+      .should('contain', `ec=contactNotes&ea=openDetails&el=Responsible%20Officer%20Change`)
+    cy.get(`[data-qa="contact-1"]`).contains('View more detail').click()
+    cy.wait('@googleAnalyticsEvent')
+      .then(data => data.request.url)
+      .should('contain', `ec=contactNotes&ea=closeDetails&el=Responsible%20Officer%20Change`)
   })
 })
