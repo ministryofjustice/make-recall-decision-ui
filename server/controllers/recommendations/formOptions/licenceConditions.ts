2db9a84c0a895acec0d70f22254d4d010d94fd36
Jon Wyatt <>
2022-08-01 21:52:43 +0100

    Remove new standard conditions

--- server/controllers/recommendations/formOptions/licenceConditions.ts
+++ server/controllers/recommendations/formOptions/licenceConditions.ts
@@ -29,19 +29,4 @@ export const standardLicenceConditions = [
     value: 'APPROVED_WORK',
     text: 'not undertake work, or a particular type of work, unless it is approved by the supervising officer and notify the supervising officer in advance of any proposal to undertake work or a particular type of work',
   },
-  {
-    id: 7,
-    value: 'APPROVED_TRAVEL',
-    text: 'not travel outside the United Kingdom, the Channel Islands or the Isle of Man except with the prior permission of your supervising officer or for the purposes of immigration deportation or removal',
-  },
-  {
-    id: 8,
-    value: 'TELL_SUPERVISOR_NAME',
-    text: 'tell your supervising officer if you use a name which is different to the name or names which appear on your licence',
-  },
-  {
-    id: 9,
-    value: 'TELL_SUPERVISOR_CONTACT_DETAILS',
-    text: 'tell your supervising officer if you change or add any contact details, including phone number or email',
-  },
 ]
