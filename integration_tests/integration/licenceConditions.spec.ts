63b5249e7d5410e97bd3d96af778c8288d0eed63
Jon Wyatt <>
2022-08-02 08:53:56 +0100

    Add test for multiple; extra assertions

--- integration_tests/integration/licenceConditions.spec.ts
+++ integration_tests/integration/licenceConditions.spec.ts
@@ -7,7 +7,7 @@ context('Licence conditions', () => {
     cy.signIn()
   })
 
-  it('can view the licence conditions page', () => {
+  it('shows conditions for a single active custodial conviction', () => {
     const crn = 'X34983'
     cy.visit(`${routeUrls.cases}/${crn}/licence-conditions`)
     cy.pageHeading().should('equal', 'Licence conditions for Charles Edwin')
@@ -16,7 +16,7 @@ context('Licence conditions', () => {
     formOptions.standardLicenceConditions.forEach(condition => cy.getElement(condition.text).should('exist'))
     // Additional licence conditions
     cy.getElement('Murder - 00100').should('exist')
-    cy.getElement({ qaAttr: 'additional-condition' }).should('have.length', 1)
+    cy.get('[data-qa="additional"] .app-summary-card').should('have.length', 1)
     cy.getElement('Supervision in the community').should('exist')
     cy.getText('condition-description').should('equal', 'Bespoke Condition (See Notes)')
     cy.getText('condition-note').should('equal', 'Must not enter Islington borough.')
@@ -31,7 +31,12 @@ context('Licence conditions', () => {
           {
             active: true,
             isCustodial: true,
-            offences: [],
+            offences: [
+              {
+                mainOffence: true,
+                description: 'Burglary - 05714',
+              },
+            ],
             licenceConditions: [{ active: false }],
           },
         ],
@@ -39,17 +44,19 @@ context('Licence conditions', () => {
     })
     const crn = 'X34983'
     cy.visit(`${routeUrls.cases}/${crn}/licence-conditions`)
-    cy.getElement('None available.').should('exist')
+    cy.getElement('Burglary - 05714').should('exist')
+    cy.getElement('There are no licence conditions available.').should('exist')
   })
 
-  it('shows a message if there are no licence conditions', () => {
+  it('shows a message if there are no active custodial convictions', () => {
     cy.task('getCase', {
       sectionId: 'licence-conditions',
       statusCode: 200,
       response: {
         convictions: [
           {
-            active: false,
+            active: true,
+            isCustodial: false,
             offences: [],
             licenceConditions: [{ active: false }],
           },
@@ -59,5 +66,90 @@ context('Licence conditions', () => {
     const crn = 'X34983'
     cy.visit(`${routeUrls.cases}/${crn}/licence-conditions`)
     cy.getElement('There are no licence conditions available.').should('exist')
+  })
+
+  it('shows conditions for multiple active custodial convictions, and a banner', () => {
+    const crn = 'X34983'
+    cy.task('getCase', {
+      sectionId: 'licence-conditions',
+      statusCode: 200,
+      response: {
+        convictions: [
+          {
+            active: true,
+            isCustodial: true,
+            offences: [
+              {
+                mainOffence: true,
+                description: 'Burglary - 05714',
+              },
+            ],
+            licenceConditions: [
+              {
+                active: true,
+                licenceConditionTypeMainCat: {
+                  description: 'Supervision in the community',
+                },
+                licenceConditionTypeSubCat: {
+                  description: 'On release to be escorted by police to Approved Premises',
+                },
+              },
+              {
+                active: true,
+                licenceConditionTypeMainCat: {
+                  description: 'Poss, own, control, inspect specified items /docs',
+                },
+                licenceConditionTypeSubCat: {
+                  description: 'tbc',
+                },
+              },
+            ],
+          },
+          {
+            active: true,
+            isCustodial: true,
+            offences: [
+              {
+                mainOffence: true,
+                description: 'Robbery',
+              },
+            ],
+            licenceConditions: [
+              {
+                active: true,
+                licenceConditionTypeMainCat: {
+                  description: 'Participate or co-op with Programme or Activities',
+                },
+                licenceConditionTypeSubCat: {
+                  description: 'Bespoke',
+                },
+              },
+            ],
+          },
+        ],
+      },
+    })
+    cy.visit(`${routeUrls.cases}/${crn}/licence-conditions`)
+    // Additional licence conditions
+    cy.getElement('Burglary - 05714').should('exist')
+    cy.getElement('Robbery').should('exist')
+    cy.getElement({ qaAttr: 'offence-heading' }).should('have.length', 2)
+    cy.get('[data-qa="additional"] .app-summary-card').should('have.length', 3)
+
+    cy.getElement('This person has 2 or more active convictions in NDelius').should('exist')
+
+    // first condition
+    cy.getElement('Poss, own, control, inspect specified items /docs', {
+      parent: '[data-qa="additional-condition-1"]',
+    }).should('exist')
+    cy.getElement('Supervision in the community', { parent: '[data-qa="additional-condition-2"]' }).should('exist')
+    cy.getElement('Participate or co-op with Programme or Activities', {
+      parent: '[data-qa="additional-condition-3"]',
+    }).should('exist')
+
+    // if no notes
+    cy.getElement('There are no notes relating to this licence condition', {
+      parent: '[data-qa="additional-condition-3"]',
+    }).should('exist')
   })
 })
