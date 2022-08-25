abec42250d816d9e7e46925816f14228b7bda003
Jon Wyatt <>
2022-08-25 11:01:31 +0100

    Show file extensions for contact documents

--- server/controllers/caseSummary/contactHistory/helpers/processContactDocuments.ts
+++ server/controllers/caseSummary/contactHistory/helpers/processContactDocuments.ts
@@ -2,22 +2,13 @@ import { CaseDocument } from '../../../../@types/make-recall-decision-api'
 import { isDefined } from '../../../../utils/utils'
 import { sortListByDateField } from '../../../../utils/dates'
 
-const getDocumentNameWithoutExtension = (documentName: string) => {
-  const extensionIdx = documentName.lastIndexOf('.')
-  return documentName.substring(0, extensionIdx)
-}
-
 export const processContactDocuments = (documents?: CaseDocument[]) => {
   if (!isDefined(documents)) {
     return undefined
   }
-  const sorted = sortListByDateField({
+  return sortListByDateField({
     list: documents,
     dateKey: 'lastModifiedAt',
     newestFirst: true,
   })
-  return sorted.map(doc => ({
-    ...doc,
-    documentNameNoExtension: getDocumentNameWithoutExtension(doc.documentName),
-  }))
 }
