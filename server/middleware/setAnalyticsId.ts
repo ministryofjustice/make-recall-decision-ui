f21eaafae9ad02bf288361278069ce45d9fb0f24
Jon Wyatt <>
2022-08-29 22:38:45 +0100

    Restore universal GA IDs (dual connect)

--- server/middleware/setAnalyticsId.ts
+++ server/middleware/setAnalyticsId.ts
@@ -4,10 +4,10 @@ export const setAnalyticsId = (req: Request, res: Response, next: NextFunction) 
   // dummy ID for testing locally / automated tests
   res.locals.googleAnalyticsId = 'UA-123456789-00'
   if (res.locals.env === 'PRODUCTION' && res.locals.flags.flagExcludeFromAnalytics !== true) {
-    res.locals.googleAnalyticsId = 'G-JD5W689LSX'
+    res.locals.googleAnalyticsId = 'UA-106741063-23'
   }
   if (res.locals.env === 'PRE-PRODUCTION') {
-    res.locals.googleAnalyticsId = 'G-4NZ883ZE66'
+    res.locals.googleAnalyticsId = 'UA-106741063-22'
   }
   next()
 }
