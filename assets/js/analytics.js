f6c61660756a6f4a85a52df1cb99cd887762ee2a
Jon Wyatt <>
2022-09-01 09:48:50 +0100

    Send events for both GA and UA

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -6,9 +6,21 @@ $('body').on('click', evt => {
     if (classList.contains('govuk-details__summary') || classList.contains('govuk-details__summary-text')) {
       const $viewDetail = $(evt.target).closest('[data-js="viewDetail"]')
       if ($viewDetail.length) {
+        let action = $viewDetail.attr('data-analytics-action')
+        if (action && $viewDetail.attr('open')) {
+          return
+        }
+        action = action || ($viewDetail.attr('open') ? 'closeDetails' : 'openDetails')
+        const category = $viewDetail.attr('data-analytics-category')
         const label = $viewDetail.attr('data-analytics-label')
+        // GA4
         gtag('event', 'select_content', {
           content_type: label,
+        })
+        // UA
+        gtag('event', category, {
+          event_category: action,
+          event_label: label,
         })
       }
     }
@@ -22,20 +34,31 @@ $('[data-js="contactTypeFiltersForm"]').on('submit', evt => {
   try {
     const $form = $(evt.target)
     $form.find('[name="contactTypes"]:checked').each((idx, el) => {
-      gtag("event", "select_item", {
+      // GA4
+      gtag('event', 'select_item', {
         item_list_name: 'contactFilterTypes',
         items: [
           {
             item_name: el.dataset.type,
             item_category: el.dataset.group,
-          }
-        ]
-      });
+          },
+        ],
+      })
+      // UA
+      gtag('event', 'contactFilterType', {
+        event_category: el.dataset.group,
+        event_label: el.dataset.type,
+      })
     })
     const searchTerm = $form.find('[name="searchFilters"]').val()
     if (searchTerm) {
+      // GA4
       gtag('event', 'search', {
         search_term: searchTerm,
+      })
+      // UA
+      gtag('event', 'contactFilterTerm', {
+        event_category: searchTerm,
       })
     }
   } catch (err) {
