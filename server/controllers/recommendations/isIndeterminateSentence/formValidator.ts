fd6ce1d44517a8a62cb22801900f4318d5a91f07
Jon Wyatt <>
2022-09-13 21:24:37 +0100

    Lint

--- server/controllers/recommendations/isIndeterminateSentence/formValidator.ts
+++ server/controllers/recommendations/isIndeterminateSentence/formValidator.ts
@@ -1,6 +1,6 @@
 import { FormValidatorArgs, FormValidatorReturn } from '../../../@types'
 import { makeErrorObject } from '../../../utils/errors'
-import { formOptions, isValueValid } from '../helpers/formOptions'
+import { isValueValid } from '../helpers/formOptions'
 import { strings } from '../../../textStrings/en'
 import { nextPageLinkUrl } from '../helpers/urls'
 
