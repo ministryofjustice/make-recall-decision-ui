2214d38e41ca683121f0e3e766a1102ab58ba956
Jon Wyatt <>
2022-09-16 11:05:08 +0100

    Lint

--- server/controllers/recommendations/recallType/formValidator.ts
+++ server/controllers/recommendations/recallType/formValidator.ts
@@ -2,7 +2,6 @@ import { FormValidatorArgs, FormValidatorReturn } from '../../../@types'
 import { makeErrorObject } from '../../../utils/errors'
 import { formOptions, isValueValid } from '../helpers/formOptions'
 import { strings } from '../../../textStrings/en'
-import { nextPageLinkUrl } from '../helpers/urls'
 
 export const validateRecallType = async ({ requestBody, urlInfo }: FormValidatorArgs): FormValidatorReturn => {
   const { recallType, recallTypeDetailsFixedTerm, recallTypeDetailsStandard } = requestBody
