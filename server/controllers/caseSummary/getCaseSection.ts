cd6cac43fb3557f61568a90c4b74055dd6cb4e7c
Jon Wyatt <>
2022-10-27 10:23:09 +0100

    Lint

--- server/controllers/caseSummary/getCaseSection.ts
+++ server/controllers/caseSummary/getCaseSection.ts
@@ -7,7 +7,7 @@ import { RiskResponse } from '../../@types/make-recall-decision-api/models/RiskR
 import { PersonDetailsResponse } from '../../@types/make-recall-decision-api/models/PersonDetailsResponse'
 import { fetchFromCacheOrApi } from '../../data/fetchFromCacheOrApi'
 import { transformContactHistory } from './contactHistory/transformContactHistory'
-import { countLabel, isCaseRestrictedOrExcluded } from '../../utils/utils'
+import { isCaseRestrictedOrExcluded } from '../../utils/utils'
 import { AppError } from '../../AppError'
 import { transformLicenceConditions } from './licenceConditions/transformLicenceConditions'
 import getRecommendationsResponse from '../../../api/responses/get-recommendations.json'
