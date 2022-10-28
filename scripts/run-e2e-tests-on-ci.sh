35807fbea0f707acc71a6897735017441928002c
Jon Wyatt <>
2022-10-28 09:53:26 +0100

    Log out parallelism

--- scripts/run-e2e-tests-on-ci.sh
+++ scripts/run-e2e-tests-on-ci.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 npm ci --no-audit
 
 SPECS=$(circleci tests glob e2e_tests/integration/*.feature | circleci tests split --total=3 | tr "\n" "," | tr " " ",")
-echo "Running feature spec(s): ${SPECS}"
+echo "Running feature spec(s): ${SPECS} with parallelism: ${<< pipeline.parameters.e2e-parallelism >>}"
 
 npx cypress run \
   --env USERNAME=${CYPRESS_USERNAME_local},PASSWORD=${CYPRESS_PASSWORD_local},CRN=${CRN_local},CRN2=${CRN2_local} \
