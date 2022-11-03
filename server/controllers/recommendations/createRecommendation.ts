42228561354efa3deddac42b38717268625f151a
Jack Maloney <jack.maloney@digital.justice.gov.uk>
2022-11-03 16:50:25 +0000

    MRD-851 Fire custom event when make a recommendation button clicked

--- server/controllers/recommendations/createRecommendation.ts
+++ server/controllers/recommendations/createRecommendation.ts
@@ -2,10 +2,15 @@ import { Request, Response } from 'express'
 import { createRecommendation } from '../../data/makeDecisionApiClient'
 import { validateCrn } from '../../utils/utils'
 import { routeUrls } from '../../routes/routeUrls'
+import { trackEvent } from '../../monitoring/azureAppInsights'
+import { EVENTS } from '../../utils/constants'
+import logger from '../../../logger'
 
 export const createRecommendationController = async (req: Request, res: Response): Promise<Response | void> => {
   const normalizedCrn = validateCrn(req.body.crn)
   try {
+    logger.info(`About to track the MRD_RECOMMENDATION_STARTED event to app insights`)
+    trackEvent(EVENTS.MRD_RECOMMENDATION_STARTED, req)
     const recommendation = await createRecommendation(normalizedCrn, res.locals.user.token)
     res.redirect(303, `${routeUrls.recommendations}/${recommendation.id}/response-to-probation`)
   } catch (err) {
---
+++ server/utils/constants.js
@@ -0,0 +1,7 @@
+const EVENTS = {
+  MRD_RECOMMENDATION_STARTED: 'mrdRecommendationStarted',
+}
+
+module.exports = Object.freeze({
+  EVENTS,
+})
