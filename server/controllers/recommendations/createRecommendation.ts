1742f3e84dcb9b16f397aaf430364990e25c3eef
Jack Maloney <jack.maloney@digital.justice.gov.uk>
2022-11-03 17:20:32 +0000

    MRD-851 Fire custom event when make a recommendation button clicked (#406)
    * MRD-851 Fire custom event when make a recommendation button clicked

--- server/controllers/recommendations/createRecommendation.ts
+++ server/controllers/recommendations/createRecommendation.ts
@@ -2,12 +2,17 @@ import { Request, Response } from 'express'
 import { createRecommendation } from '../../data/makeDecisionApiClient'
 import { validateCrn } from '../../utils/utils'
 import { routeUrls } from '../../routes/routeUrls'
+import { trackEvent } from '../../monitoring/azureAppInsights'
+import { EVENTS } from '../../utils/constants'
+import logger from '../../../logger'
 
 export const createRecommendationController = async (req: Request, res: Response): Promise<Response | void> => {
   const normalizedCrn = validateCrn(req.body.crn)
   try {
+    logger.info(`About to track the MRD_RECOMMENDATION_STARTED event to app insights`)
     const recommendation = await createRecommendation(normalizedCrn, res.locals.user.token)
     res.redirect(303, `${routeUrls.recommendations}/${recommendation.id}/response-to-probation`)
+    trackEvent(EVENTS.MRD_RECOMMENDATION_STARTED, req)
   } catch (err) {
     req.session.errors = [
       {
