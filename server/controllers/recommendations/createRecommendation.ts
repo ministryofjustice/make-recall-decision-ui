aa733bdce72667423b96cc3a21d67107953bc173
Jack Maloney <jack.maloney@digital.justice.gov.uk>
2022-11-04 12:47:23 +0000

    MRD-851 Add logging for app insights (#408)
    Co-authored-by: Jon Wyatt <jonnywyatt2@gmail.com>

--- server/controllers/recommendations/createRecommendation.ts
+++ server/controllers/recommendations/createRecommendation.ts
@@ -9,9 +9,9 @@ import logger from '../../../logger'
 export const createRecommendationController = async (req: Request, res: Response): Promise<Response | void> => {
   const normalizedCrn = validateCrn(req.body.crn)
   try {
-    logger.info(`About to track the MRD_RECOMMENDATION_STARTED event to app insights`)
     const recommendation = await createRecommendation(normalizedCrn, res.locals.user.token)
     res.redirect(303, `${routeUrls.recommendations}/${recommendation.id}/response-to-probation`)
+    logger.info(`Sending track event from create recommendation controller`)
     trackEvent(EVENTS.MRD_RECOMMENDATION_STARTED, req)
   } catch (err) {
     req.session.errors = [
