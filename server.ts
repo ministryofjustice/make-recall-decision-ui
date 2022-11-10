9cfb0dec3c71c91efa460bc0400d3487ce9553f0
Jon Wyatt <>
2022-11-10 11:21:17 +0000

    Commented instructions to run localhost with https

--- server.ts
+++ server.ts
@@ -18,3 +18,15 @@ app.listen(app.get('port'), () => {
 metricsApp.listen(metricsApp.get('port'), () => {
   logger.info(`Metrics server listening on port ${metricsApp.get('port')}`)
 })
+
+// ================================= run localhost with https ===============================
+// 1. brew install mkcert
+// 2. mkcert -install
+// 3. mkcert localhost
+// then uncomment the following code, and comment out the app.listen block above
+// const fs = require('fs')
+// const https = require('https')
+//
+// const key = fs.readFileSync('localhost-key.pem', 'utf-8')
+// const cert = fs.readFileSync('localhost.pem', 'utf-8')
+// https.createServer({ key, cert }, app).listen(app.get('port'))
