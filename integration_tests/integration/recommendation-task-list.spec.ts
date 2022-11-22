e4932930cf3f310c213bfe0be7f221a4c7e2a7da
Jon Wyatt <>
2022-11-22 11:40:10 +0000

    Add flags to URLs

--- integration_tests/integration/recommendation-task-list.spec.ts
+++ integration_tests/integration/recommendation-task-list.spec.ts
@@ -40,7 +40,7 @@ context('Recommendation - task list', () => {
 
   it('task list - Completed - in custody', () => {
     cy.task('getRecommendation', { statusCode: 200, response: completeRecommendationResponse })
-    cy.visit(`${routeUrls.recommendations}/${recommendationId}/task-list`)
+    cy.visit(`${routeUrls.recommendations}/${recommendationId}/task-list?flagRecommendationOffenceDetails=1`)
     cy.getElement('What you recommend Completed').should('exist')
     cy.getElement('What alternatives to recall have been tried already? Completed').should('exist')
     cy.getElement('How has Paula Smith responded to probation so far? Completed').should('exist')
@@ -92,7 +92,7 @@ context('Recommendation - task list', () => {
       statusCode: 200,
       response: recommendationResponse,
     })
-    cy.visit(`${routeUrls.recommendations}/${recommendationId}/task-list`)
+    cy.visit(`${routeUrls.recommendations}/${recommendationId}/task-list?flagRecommendationOffenceDetails=1`)
     cy.getElement('What you recommend Completed').should('exist')
     cy.getElement('What alternatives to recall have been tried already? To do').should('exist')
     cy.getElement('How has Paula Smith responded to probation so far? To do').should('exist')
