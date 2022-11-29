cf4d2a6bad66c38dceddeeeef499b0a598eae39e
Jon Wyatt <>
2022-11-29 11:01:59 +0000

    Remove int test assertion

--- integration_tests/integration/recommendation-task-list.spec.ts
+++ integration_tests/integration/recommendation-task-list.spec.ts
@@ -54,7 +54,6 @@ context('Recommendation - task list', () => {
     cy.getElement('Type of indeterminate sentence Completed').should('exist')
     cy.getElement('Confirm the recall criteria - indeterminate and extended sentences Completed').should('exist')
     cy.getElement('Personal details Reviewed').should('exist')
-    cy.getElement('Offence details Reviewed').should('exist')
     // the following 2 links should not be present, as person is in custody
     cy.getElement('Local police contact details').should('not.exist')
     cy.getElement('Is there anything the police should know before they arrest Paula Smith?').should('not.exist')
