78deccc1535b09778e4016a425fba16576df9940
Jon Wyatt <>
2022-12-23 11:28:59 +0000

    Redirect to case overview if recommendation status is DOCUMENT_DOWNLOADED

--- server/controllers/recommendations/helpers/urls.test.ts
+++ server/controllers/recommendations/helpers/urls.test.ts
@@ -1,5 +1,6 @@
-import { nextPageLinkUrl, changeLinkUrl, validateUpdateRecommendationPageRequest } from './urls'
+import { nextPageLinkUrl, changeLinkUrl, checkForRedirectPath } from './urls'
 import { RecallTypeSelectedValue } from '../../../@types/make-recall-decision-api'
+import { RecommendationResponse } from '../../../@types/make-recall-decision-api/models/RecommendationResponse'
 
 describe('nextPageLinkUrl', () => {
   it('returns a url back to the "from" page, if supplied', () => {
@@ -52,73 +53,117 @@ describe('changeLinkUrl', () => {
   })
 })
 
-describe('validateUpdateRecommendationPageRequest', () => {
+describe('checkForRedirectPath', () => {
+  const basePath = '/recommendations/123/'
+  const crn = 'X514364'
+
   it('returns null if neither task list was requested', () => {
-    const pageUrlSlug = validateUpdateRecommendationPageRequest({
+    const pageUrlSlug = checkForRedirectPath({
       requestedPageId: 'emergency-recall',
       recallType: 'FIXED_TERM' as RecallTypeSelectedValue.value,
+      basePathRecFlow: basePath,
+      crn,
+      recommendationStatus: RecommendationResponse.status.DRAFT,
     })
     expect(pageUrlSlug).toBeNull()
   })
 
   it('returns null if recall task list requested for standard recall', () => {
-    const pageUrlSlug = validateUpdateRecommendationPageRequest({
+    const pageUrlSlug = checkForRedirectPath({
       requestedPageId: 'task-list',
       recallType: 'STANDARD' as RecallTypeSelectedValue.value,
+      basePathRecFlow: basePath,
+      crn,
+      recommendationStatus: RecommendationResponse.status.DRAFT,
     })
     expect(pageUrlSlug).toBeNull()
   })
 
   it('returns null if recall task list requested for fixed term recall', () => {
-    const pageUrlSlug = validateUpdateRecommendationPageRequest({
+    const pageUrlSlug = checkForRedirectPath({
       requestedPageId: 'task-list',
       recallType: 'FIXED_TERM' as RecallTypeSelectedValue.value,
+      basePathRecFlow: basePath,
+      crn,
+      recommendationStatus: RecommendationResponse.status.DRAFT,
     })
     expect(pageUrlSlug).toBeNull()
   })
 
   it('returns null if no recall task list requested for no recall', () => {
-    const pageUrlSlug = validateUpdateRecommendationPageRequest({
+    const pageUrlSlug = checkForRedirectPath({
       requestedPageId: 'task-list-no-recall',
       recallType: 'NO_RECALL' as RecallTypeSelectedValue.value,
+      basePathRecFlow: basePath,
+      crn,
+      recommendationStatus: RecommendationResponse.status.DRAFT,
     })
     expect(pageUrlSlug).toBeNull()
   })
 
   it('returns no recall task list if recall task list requested for no recall', () => {
-    const pageUrlSlug = validateUpdateRecommendationPageRequest({
+    const pageUrlSlug = checkForRedirectPath({
       requestedPageId: 'task-list',
       recallType: 'NO_RECALL' as RecallTypeSelectedValue.value,
+      basePathRecFlow: basePath,
+      crn,
+      recommendationStatus: RecommendationResponse.status.DRAFT,
     })
-    expect(pageUrlSlug).toEqual('task-list-no-recall')
+    expect(pageUrlSlug).toEqual(`${basePath}task-list-no-recall`)
   })
 
   it('returns recall task list if no recall task list requested for standard recall', () => {
-    const pageUrlSlug = validateUpdateRecommendationPageRequest({
+    const pageUrlSlug = checkForRedirectPath({
       requestedPageId: 'task-list-no-recall',
       recallType: 'STANDARD' as RecallTypeSelectedValue.value,
+      basePathRecFlow: basePath,
+      crn,
+      recommendationStatus: RecommendationResponse.status.DRAFT,
     })
-    expect(pageUrlSlug).toEqual('task-list')
+    expect(pageUrlSlug).toEqual(`${basePath}task-list`)
   })
 
   it('returns recall task list if no recall task list requested for fixed term recall', () => {
-    const pageUrlSlug = validateUpdateRecommendationPageRequest({
+    const pageUrlSlug = checkForRedirectPath({
       requestedPageId: 'task-list-no-recall',
       recallType: 'FIXED_TERM' as RecallTypeSelectedValue.value,
+      basePathRecFlow: basePath,
+      crn,
+      recommendationStatus: RecommendationResponse.status.DRAFT,
     })
-    expect(pageUrlSlug).toEqual('task-list')
+    expect(pageUrlSlug).toEqual(`${basePath}task-list`)
   })
 
   it('returns response to probation if recall task list requested and recall not set', () => {
-    const pageUrlSlug = validateUpdateRecommendationPageRequest({ requestedPageId: 'task-list', recallType: undefined })
-    expect(pageUrlSlug).toEqual('response-to-probation')
+    const pageUrlSlug = checkForRedirectPath({
+      requestedPageId: 'task-list',
+      recallType: undefined,
+      basePathRecFlow: basePath,
+      crn,
+      recommendationStatus: RecommendationResponse.status.DRAFT,
+    })
+    expect(pageUrlSlug).toEqual(`${basePath}response-to-probation`)
   })
 
   it('returns response to probation if no recall task list requested and recall not set', () => {
-    const pageUrlSlug = validateUpdateRecommendationPageRequest({
+    const pageUrlSlug = checkForRedirectPath({
       requestedPageId: 'task-list-no-recall',
       recallType: undefined,
+      basePathRecFlow: basePath,
+      crn,
+      recommendationStatus: RecommendationResponse.status.DRAFT,
     })
-    expect(pageUrlSlug).toEqual('response-to-probation')
+    expect(pageUrlSlug).toEqual(`${basePath}response-to-probation`)
+  })
+
+  it('returns case summary overview if recommendation status is DOCUMENT_DOWNLOADED', () => {
+    const pageUrlSlug = checkForRedirectPath({
+      requestedPageId: 'task-list-no-recall',
+      recallType: undefined,
+      basePathRecFlow: basePath,
+      crn,
+      recommendationStatus: RecommendationResponse.status.DOCUMENT_DOWNLOADED,
+    })
+    expect(pageUrlSlug).toEqual(`/cases/${crn}/overview`)
   })
 })
