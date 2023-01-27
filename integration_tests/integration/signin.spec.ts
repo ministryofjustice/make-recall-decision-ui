86d3514f5552b19d5c4aabb986aaf45167efc7ce
Jon Wyatt <jonnywyatt2@gmail.com>
2023-01-27 10:29:31 +0000

    MRD-1070 - a11 fix - add opens in new tab warnings (#567)
    Co-authored-by: Jon Wyatt <>

--- integration_tests/integration/signin.spec.ts
+++ integration_tests/integration/signin.spec.ts
@@ -29,7 +29,7 @@ context('SignIn', () => {
   it('User can manage their details and give feedback', () => {
     cy.signIn()
     cy.getLinkHref('J. Smith').should('contain', '/account-details')
-    cy.getText('feedback-form-link').should('equal', 'feedback')
+    cy.getText('feedback-form-link').should('equal', 'feedback Opens in a new tab')
     cy.getLinkHref({ qaAttr: 'feedback-form-link' }).should(
       'contain',
       'https://docs.google.com/forms/d/1ZJPD40s-tzr9Uf0HwrrF0YP9bUilCe228Xk-2d2quNE/edit?ts=62d7ca55'
