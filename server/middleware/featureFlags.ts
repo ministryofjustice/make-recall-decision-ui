bb05ae8cae4dc20f9fd1a7d629d2b09d42ce33fc
Jon Wyatt <jonnywyatt2@gmail.com>
2023-01-30 16:32:04 +0000

    MRD-1110 - Add descriptions on feature flags page (#575)
    Co-authored-by: Jon Wyatt <>

--- server/middleware/featureFlags.ts
+++ server/middleware/featureFlags.ts
@@ -5,26 +5,37 @@ import { FeatureFlagDefault } from '../@types/featureFlags'
 export const featureFlagsDefaults = {
   flagConsiderRecall: {
     label: 'Consider a recall / manager decision',
+    description:
+      'POs can enter a "Consider a recall" rationale. They are then blocked from progressing past the "Review with a manager" page until an SPO records their decision.',
     default: false,
   },
   flagDomainEventRecommendationStarted: {
-    label: "Create Delius contact when user clicks 'Make recommendation' button",
+    label: 'Create NDelius contact when user creates a recommendation',
+    description: 'Adds a contact in NDelius when a new recommendation is created',
     default: false,
   },
   flagRecommendationsPage: {
-    label: 'Recommendations list',
+    label: 'Recommendations tab',
+    description:
+      'Shows a "Recommendations" tab in Case summary, with a list of all recommendations that have been created for that CRN',
     default: false,
   },
   flagDeleteRecommendation: {
-    label: 'Allow (soft) deletion of a recommendation',
+    label: 'Allow deletion of a recommendation',
+    description:
+      'Development team use only - shows links on the Recommendations tab allowing any recommendation to be marked as deleted. Deleting a recommendation allows a new one to be created, if needed. The "deleted" recommendation will be retained in the database, and no data or audit info will be lost.',
     default: false,
   },
   flagExcludeFromAnalytics: {
     label: "Don't send events to Google Analytics or App Insights for my activity",
     default: false,
+    description:
+      'Development team use only - allows you to browse and use the product without creating analytics data. Note - audit data will still be created.',
   },
   flagCreateVaryLicenceData: {
     label: "Show tab for 'Create & vary a licence' data",
+    description:
+      'Development team use only - shows a tab on the Case summary showing data from the "Create & vary a licence" service',
     default: false,
   },
 }
