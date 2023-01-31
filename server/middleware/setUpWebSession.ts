5cf012ce981614b271f8e702cb3f7118d94d1e64
Jon Wyatt <jonnywyatt2@gmail.com>
2023-01-31 14:30:48 +0000

    Upgrade to Redis 4 (#577)
    Co-authored-by: Jon Wyatt <>

--- server/middleware/setUpWebSession.ts
+++ server/middleware/setUpWebSession.ts
@@ -1,17 +1,20 @@
 import { v4 as uuidv4 } from 'uuid'
 import session from 'express-session'
-import connectRedis from 'connect-redis'
+import connectRedis, { Client } from 'connect-redis'
 import express, { Router } from 'express'
 import { createRedisClient } from '../data/redisClient'
 import config from '../config'
+import logger from '../../logger'
 
 const RedisStore = connectRedis(session)
 
 export default function setUpWebSession(): Router {
+  const client = createRedisClient({ legacyMode: true })
+  client.connect().catch((err: Error) => logger.error(`Error connecting to Redis`, err))
   const router = express.Router()
   router.use(
     session({
-      store: new RedisStore({ client: createRedisClient() }),
+      store: new RedisStore({ client: client as unknown as Client }),
       cookie: { secure: config.https, sameSite: 'lax', maxAge: config.session.expiryMinutes * 60 * 1000 },
       secret: config.session.secret,
       resave: false, // redis implements touch so shouldn't need this
