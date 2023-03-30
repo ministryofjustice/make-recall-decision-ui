d5f3efe4a788317ab72a88936ccf673a25840722
Robin de Villiers <robin.devilliers@digital.justice.gov.uk>
2023-03-30 17:21:08 +0100

    MRD-1137 - error handling fix

--- server/routes/recommendations.ts
+++ server/routes/recommendations.ts
@@ -78,9 +78,9 @@ function routeRecommendationGet(endpoint: string, routerCallback: RouterCallback
     `/:recommendationId/${endpoint}`,
     sanitizeInputValues,
     parseRecommendationUrl,
-    retrieve,
+    feedErrorsToExpress(retrieve), // necessary for async functions
     customizeMessages,
-    routerCallback,
+    feedErrorsToExpress(routerCallback), // necessary for async functions
     audit,
     (error: Error, req: Request, res: Response, next: NextFunction): void => {
       next(error) // forward errors to root router
@@ -93,9 +93,19 @@ function routeRecommendationPost(endpoint: string, routerCallback: RouterCallbac
     `/:recommendationId/${endpoint}`,
     sanitizeInputValues,
     parseRecommendationUrl,
-    routerCallback,
+    feedErrorsToExpress(routerCallback), // necessary for async functions
     (error: Error, req: Request, res: Response, next: NextFunction): void => {
       next(error) // forward errors to root router
     }
   )
+}
+
+function feedErrorsToExpress(routerCallback: RouterCallback) {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await routerCallback(req, res, next)
+    } catch (err) {
+      next(err)
+    }
+  }
 }
