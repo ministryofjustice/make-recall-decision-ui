6d92e147c5a19f94e15439bb154399a1359573e5
robinandrewdevilliers <127398712+robinandrewdevilliers@users.noreply.github.com>
2023-05-15 09:14:46 +0100

    MRD-1320 - SP countersignature page (#672)

--- server/controllers/recommendation/redirectController.test.ts
+++ server/controllers/recommendation/redirectController.test.ts
@@ -1,6 +1,7 @@
 import { mockNext, mockReq, mockRes } from '../../middleware/testutils/mockRequestUtils'
 import redirectController from './redirectController'
 import { getStatuses, updateStatuses } from '../../data/makeDecisionApiClient'
+import { STATUSES } from '../../middleware/recommendationStatusCheck'
 
 jest.mock('../../data/makeDecisionApiClient')
 
@@ -95,7 +96,7 @@ describe('get', () => {
     expect(next).toHaveBeenCalled()
   })
 
-  it('redirect to spo-task-list-consider-recall', async () => {
+  it('redirect to spo-task-list-consider-recall by default if spo', async () => {
     ;(getStatuses as jest.Mock).mockResolvedValue([])
     const res = mockRes({
       locals: {
@@ -148,7 +149,7 @@ describe('get', () => {
     expect(next).toHaveBeenCalled()
   })
 
-  it('redirect to task-list-no-recall', async () => {
+  it('redirect to task-list-no-recall if SPO_CONSIDER_RECALL', async () => {
     ;(getStatuses as jest.Mock).mockResolvedValue([{ name: 'SPO_CONSIDER_RECALL', active: true }])
     const res = mockRes({
       locals: {
@@ -172,6 +173,87 @@ describe('get', () => {
     expect(updateStatuses).not.toHaveBeenCalled()
 
     expect(res.redirect).toHaveBeenCalledWith(301, '/recommendation/123/task-list-no-recall')
+    expect(next).toHaveBeenCalled()
+  })
+
+  it('redirect to task-list-no-recall if SPO_CONSIDERING_RECALL', async () => {
+    ;(getStatuses as jest.Mock).mockResolvedValue([{ name: 'SPO_CONSIDERING_RECALL', active: true }])
+    const res = mockRes({
+      locals: {
+        recommendation: {
+          recallType: {
+            selected: {
+              value: 'NO_RECALL',
+            },
+          },
+        },
+        urlInfo: { basePath: '/recommendation/123/' },
+        user: {
+          token: 'token1',
+          roles: ['ROLE_MAKE_RECALL_DECISION'],
+        },
+      },
+    })
+    const next = mockNext()
+    await redirectController.get(mockReq(), res, next)
+
+    expect(updateStatuses).not.toHaveBeenCalled()
+
+    expect(res.redirect).toHaveBeenCalledWith(301, '/recommendation/123/task-list-no-recall')
+    expect(next).toHaveBeenCalled()
+  })
+
+  it('redirect to task-list if SPO_SIGNATURE_REQUESTED', async () => {
+    ;(getStatuses as jest.Mock).mockResolvedValue([{ name: STATUSES.SPO_SIGNATURE_REQUESTED, active: true }])
+    const res = mockRes({
+      locals: {
+        recommendation: {
+          recallType: {
+            selected: {
+              value: 'STANDARD',
+            },
+          },
+        },
+        urlInfo: { basePath: '/recommendation/123/' },
+        user: {
+          token: 'token1',
+          roles: ['ROLE_MAKE_RECALL_DECISION'],
+        },
+      },
+    })
+    const next = mockNext()
+    await redirectController.get(mockReq(), res, next)
+
+    expect(updateStatuses).not.toHaveBeenCalled()
+
+    expect(res.redirect).toHaveBeenCalledWith(301, '/recommendation/123/task-list')
+    expect(next).toHaveBeenCalled()
+  })
+
+  it('redirect to task-list if ACO_SIGNATURE_REQUESTED', async () => {
+    ;(getStatuses as jest.Mock).mockResolvedValue([{ name: STATUSES.ACO_SIGNATURE_REQUESTED, active: true }])
+    const res = mockRes({
+      locals: {
+        recommendation: {
+          recallType: {
+            selected: {
+              value: 'STANDARD',
+            },
+          },
+        },
+        urlInfo: { basePath: '/recommendation/123/' },
+        user: {
+          token: 'token1',
+          roles: ['ROLE_MAKE_RECALL_DECISION'],
+        },
+      },
+    })
+    const next = mockNext()
+    await redirectController.get(mockReq(), res, next)
+
+    expect(updateStatuses).not.toHaveBeenCalled()
+
+    expect(res.redirect).toHaveBeenCalledWith(301, '/recommendation/123/task-list')
     expect(next).toHaveBeenCalled()
   })
 })
