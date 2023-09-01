de122d3568c2b1c10a666454fed0e4ac36b92e6f
Bill Sclater <bill.sclater@digital.justice.gov.uk>
2023-09-01 09:31:38 +0100

    Revert "mrd-1670_irsa-changes: further irsa updates (#832)"
    This reverts commit 9c1ad860d4b9b9584d465d1cddd0bd1b49a37e3c.

--- server/config.ts
+++ server/config.ts
@@ -78,6 +78,9 @@ export default {
     },
     audit: {
       region: get('AUDIT_SQS_REGION', 'eu-west-2', requiredInProduction),
+      accessKeyId: get('AUDIT_SQS_ACCESS_KEY_ID', 'foobar', requiredInProduction),
+      secretAccessKey: get('AUDIT_SQS_SECRET_ACCESS_KEY', 'foobar', requiredInProduction),
+      endpoint: production ? null : 'http://localhost:4566',
       queueUrl: get('AUDIT_SQS_QUEUE_URL', 'foobar', requiredInProduction),
       serviceName: get('AUDIT_SERVICE_NAME', 'make-recall-decision-ui', requiredInProduction),
     },
