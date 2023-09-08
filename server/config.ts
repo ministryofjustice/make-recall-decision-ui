01d7e6e91c4f68f01e6451a55dc013b72078237b
bills7281 <104494371+bills7281@users.noreply.github.com>
2023-09-08 14:44:54 +0100

    mrd-1491_config-correction-maintenance-banner: added undefined as fal… (#851)
    * mrd-1491_config-correction-maintenance-banner: added undefined as fallback for maintenance banner env vars to allow empty strings for text
    
    * mrd-1491_config-correction-maintenance-banner: changed fallback to null to allow env checks to pass
    
    ---------
    
    Co-authored-by: Bill Sclater <bill.sclater@justice.gov.uk>

--- server/config.ts
+++ server/config.ts
@@ -94,7 +94,7 @@ export default {
   displayMaintenancePage: get('DISPLAY_MAINTENANCE_PAGE', false, requiredInProduction),
   maintenancePageText: get('MAINTENANCE_PAGE_TEXT', null),
   notification: {
-    active: get('SHOW_NOTIFICATION', false, requiredInProduction),
+    active: get('SHOW_NOTIFICATION', null, requiredInProduction),
     header: get('NOTIFICATION_HEADER', null, { requireInProduction: true }),
     body: get('NOTIFICATION_BODY', null, { requireInProduction: true }),
   },
