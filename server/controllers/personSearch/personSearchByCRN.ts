a4b0f920fb6db9a208c212c1df125983502b4c1a
bills7281 <104494371+bills7281@users.noreply.github.com>
2023-09-13 10:16:28 +0100

    mrd-1491_maintenance-banner-styling-and-screens-fix: banner removed f… (#855)
    * mrd-1491_maintenance-banner-styling-and-screens-fix: banner removed from search screens
    
    * mrd-1491_maintenance-banner-styling-and-screens-fix: banner removed from rec-flow screens
    
    * mrd-1491_maintenance-banner-styling-and-screens-fix: banner template removed from root layout
    
    * mrd-1491_maintenance-banner-styling-and-screens-fix: introduced banner to start page
    
    * mrd-1491_maintenance-banner-styling-and-screens-fix: introduced banner to case sumary page

--- server/controllers/personSearch/personSearchByCRN.ts
+++ server/controllers/personSearch/personSearchByCRN.ts
@@ -1,11 +1,6 @@
 import { Request, Response } from 'express'
-import config from '../../config'
 
 export const personSearchByCRN = async (req: Request, res: Response): Promise<Response | void> => {
-  res.locals.notification = {
-    ...config.notification,
-    isVisible: String(config.notification.active).toLowerCase() === 'true' && Boolean(config.notification.body),
-  }
   const { flags } = res.locals
   if (flags.flagSearchByName) {
     res.render('pages/personSearchByCRN')
