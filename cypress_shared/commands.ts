97a718904ec2447a3c6d0b63b01649ec2cde1ace
Darren Walker <Darren.Walker@digital.justice.gov.uk>
2023-10-02 17:03:35 +0100

    Delete e2e tests, which are now in their own repo

--- cypress_shared/commands.ts
+++ cypress_shared/commands.ts
@@ -4,6 +4,84 @@ import {
   replaceMissingNDeliusInfoWithNotSpecified,
 } from './utils'
 
+Cypress.Keyboard.defaults({
+  keystrokeDelay: 0,
+})
+
+export enum UserType {
+  PO = 'PO',
+  SPO = 'SPO',
+  ACO = 'ACO',
+}
+
+const userName = Cypress.env('USERNAME')
+const password = Cypress.env('PASSWORD')
+
+const userNameSpo = Cypress.env('USERNAME_SPO')
+const passwordSpo = Cypress.env('PASSWORD_SPO')
+
+const userNameAco = Cypress.env('USERNAME_ACO')
+const passwordAco = Cypress.env('PASSWORD_ACO')
+
+const getUserDetails = function (userType: UserType) {
+  const userDetails = {
+    name: '',
+    username: '',
+    email: '',
+  }
+  cy.visit('/account-details')
+  cy.getText('name').then(text => {
+    userDetails.name = text
+  })
+  cy.getText('username').then(text => {
+    userDetails.username = text
+  })
+  cy.getText('email').then(text => {
+    userDetails.email = text
+  })
+  cy.go('back')
+  return cy.wrap(userDetails).as(UserType[userType])
+}
+
+Cypress.Commands.add('visitPage', (url, isSpoUser = false) => {
+  cy.clearCookies()
+  cy.visit(url)
+  cy.pageHeading().should('equal', 'Sign in')
+  cy.get('#username').type(isSpoUser ? userNameSpo : userName, { log: false })
+  cy.get('#password').type(isSpoUser ? passwordSpo : password, { log: false })
+  cy.get('#submit').click()
+  getUserDetails(isSpoUser ? UserType.SPO : UserType.PO)
+})
+
+Cypress.Commands.add('visitPageAndLogin', function (url, userType = UserType.PO) {
+  cy.clearCookies()
+  cy.visit(url)
+  cy.pageHeading().should('equal', 'Sign in')
+  let user: string
+  let pw: string
+  switch (userType) {
+    case UserType.ACO: {
+      user = userNameAco
+      pw = passwordAco
+      break
+    }
+    case UserType.SPO: {
+      user = userNameSpo
+      pw = passwordSpo
+      break
+    }
+    default: {
+      user = userName
+      pw = password
+      break
+    }
+  }
+  cy.get('#username').type(user, { log: false })
+  cy.get('#password').type(pw, { log: false })
+  cy.get('#submit').click()
+  getUserDetails(userType)
+})
+
 Cypress.Commands.add('pageHeading', () =>
   cy
     .get('h1')
