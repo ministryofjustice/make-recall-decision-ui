81e0cd85a399d6390a780ba4f0546678df9a402b
Bill Sclater <bill.sclater@digital.justice.gov.uk>
2023-10-11 14:42:19 +0100

    mrd-1015_replay-answers-what-has-led-to-this-recall: added replay answer boxes to `what has led to this recall?` screen

--- assets/js/copyToClipboard.js
+++ assets/js/copyToClipboard.js
@@ -3,16 +3,33 @@ $('[data-js="copy-to-clipboard-btn"]').on('click', (evt) => {
   const $ariaLiveRegion = $('[data-js="aria-live-region"]')
   const showError = () => $ariaLiveRegion.text('Unable to copy text')
   const text = $('[data-js="copy-to-clipboard-text"]').text()
-  if (!text) {
-    showError()
-  } else {
-    navigator.clipboard.writeText(text).then(
-      () => {
-        $ariaLiveRegion.text('Text copied to clipboard')
-  const $button = $(evt.target)
-        $button.text('Text copied')
-      },
-      showError
-    );
+  const text2 = $('[data-js="copy-to-clipboard-text2"]').text()
+  if (evt.delegateTarget.id === 'copy-to-clipboard-text') {
+    if (!text) {
+      showError()
+    } else {
+      navigator.clipboard.writeText(text).then(
+        () => {
+          $ariaLiveRegion.text('Text copied to clipboard')
+          const $button = $(evt.target)
+          $button.text('Text copied')
+        },
+        showError
+      );
+    }
+  }
+  if (evt.delegateTarget.id === 'copy-to-clipboard-text2') {
+    if (!text2) {
+      showError()
+    } else {
+      navigator.clipboard.writeText(text2).then(
+        () => {
+          $ariaLiveRegion.text('Text copied to clipboard')
+          const $button = $(evt.target)
+          $button.text('Text copied')
+        },
+        showError
+      );
+    }
   }
 })
