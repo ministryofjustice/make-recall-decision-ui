b41b49f4e2bd59fcd21c0aeca37916f9a392d072
bills7281 <104494371+bills7281@users.noreply.github.com>
2023-10-13 11:20:36 +0100

    mrd-1015_replay-answers-what-has-led-to-this-recall: added replay ans… (#917)
    * mrd-1015_replay-answers-what-has-led-to-this-recall: added replay answer boxes to `what has led to this recall?` screen
    
    * MRD-1015 Refactor copy to clipboard JS
    
    ---------
    
    Co-authored-by: Darren Walker <Darren.Walker@digital.justice.gov.uk>

--- assets/js/copyToClipboard.js
+++ assets/js/copyToClipboard.js
@@ -1,18 +1,25 @@
-
-$('[data-js="copy-to-clipboard-btn"]').on('click', (evt) => {
+// How to use:
+// For pages with one "copy to clipboard" button:
+// - assign an attribute data-js='copy-to-clipboard-text' to the text element
+// - assign an attribute data-js='copy-to-clipboard-btn' to the button
+// For pages with multiple "copy to clipboard" buttons:
+// - make the data-js attribute on the text elements unique
+// - assign an id to the button that matches the content of the data-js on the text element
+$('[data-js="copy-to-clipboard-btn"]').on('click', evt => {
   const $ariaLiveRegion = $('[data-js="aria-live-region"]')
   const showError = () => $ariaLiveRegion.text('Unable to copy text')
-  const text = $('[data-js="copy-to-clipboard-text"]').text()
+  let buttonId = 'copy-to-clipboard-text'
+  if (!!evt.delegateTarget.id) {
+    buttonId = evt.delegateTarget.id
+  }
+  const text = $(`[data-js="${buttonId}"]`).text()
   if (!text) {
     showError()
   } else {
-    navigator.clipboard.writeText(text).then(
-      () => {
-        $ariaLiveRegion.text('Text copied to clipboard')
-  const $button = $(evt.target)
-        $button.text('Text copied')
-      },
-      showError
-    );
+    navigator.clipboard.writeText(text).then(() => {
+      $ariaLiveRegion.text('Text copied to clipboard')
+      const $button = $(evt.target)
+      $button.text('Text copied')
+    }, showError)
   }
 })
