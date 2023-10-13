d7e488f2cb987b18277bcca454feea5bdd866ce6
Darren Walker <Darren.Walker@digital.justice.gov.uk>
2023-10-13 10:54:21 +0100

    MRD-1015 Refactor copy to clipboard JS

--- assets/js/copyToClipboard.js
+++ assets/js/copyToClipboard.js
@@ -1,35 +1,25 @@
-
-$('[data-js="copy-to-clipboard-btn"]').on('click', (evt) => {
+// How to use:
+// For pages with one "copy to clipboard" button:
+// - assign an attribute data-js='copy-to-clipboard-text' to the text element
+// - assign an attribute data-js='copy-to-clipboard-btn' to the button
+// For pages with multiple "copy to clipboard" buttons:
+// - make the data-js attribute on the text elements unique
+// - assign an id to the button that matches the content of the data-js on the text element
+$('[data-js="copy-to-clipboard-btn"]').on('click', evt => {
   const $ariaLiveRegion = $('[data-js="aria-live-region"]')
   const showError = () => $ariaLiveRegion.text('Unable to copy text')
-  const text = $('[data-js="copy-to-clipboard-text"]').text()
-  const text2 = $('[data-js="copy-to-clipboard-text2"]').text()
-  if (evt.delegateTarget.id === 'copy-to-clipboard-text') {
-    if (!text) {
-      showError()
-    } else {
-      navigator.clipboard.writeText(text).then(
-        () => {
-          $ariaLiveRegion.text('Text copied to clipboard')
-          const $button = $(evt.target)
-          $button.text('Text copied')
-        },
-        showError
-      );
-    }
+  let buttonId = 'copy-to-clipboard-text'
+  if (!!evt.delegateTarget.id) {
+    buttonId = evt.delegateTarget.id
   }
-  if (evt.delegateTarget.id === 'copy-to-clipboard-text2') {
-    if (!text2) {
-      showError()
-    } else {
-      navigator.clipboard.writeText(text2).then(
-        () => {
-          $ariaLiveRegion.text('Text copied to clipboard')
-          const $button = $(evt.target)
-          $button.text('Text copied')
-        },
-        showError
-      );
-    }
+  const text = $(`[data-js="${buttonId}"]`).text()
+  if (!text) {
+    showError()
+  } else {
+    navigator.clipboard.writeText(text).then(() => {
+      $ariaLiveRegion.text('Text copied to clipboard')
+      const $button = $(evt.target)
+      $button.text('Text copied')
+    }, showError)
   }
 })
