14a4cdffdcec2b61639df974caa12887be95968a
bills7281 <104494371+bills7281@users.noreply.github.com>
2023-10-25 15:36:18 +0100

    mrd-1975_ppcs-logins: PPCS role added with first screen of journey (#943)
    * mrd-1975_ppcs-logins: PPCS role added with first screen of journey
    
    * Update startPage.test.ts
    
    * mrd-1975_ppcs-logins: renamed role and user to follow existing pattern

--- server/controllers/startPage/startPage.ts
+++ server/controllers/startPage/startPage.ts
@@ -1,12 +1,25 @@
 import { Request, Response } from 'express'
+import jwtDecode from 'jwt-decode'
 import config from '../../config'
 import { isBannerDisplayDateRangeValid } from '../../utils/utils'
+
+export enum HMPPS_AUTH_ROLE {
+  PPCS = 'ROLE_MAKE_RECALL_DECISION_PPCS',
+}
 
 export const startPage = async (req: Request, res: Response): Promise<Response | void> => {
   res.locals.notification = {
     ...config.notification,
     isVisible: Boolean(config.notification.body) && isBannerDisplayDateRangeValid(),
   }
-  res.locals.searchEndpoint = '/search-by-name'
-  res.render('pages/startPage')
+  const { authorities: roles = [] } = jwtDecode(res.locals.user.token) as { authorities?: string[] }
+  res.locals.user.roles = roles
+  res.locals.user.hasPpcsRole = roles.includes(HMPPS_AUTH_ROLE.PPCS)
+  if (res.locals.user.hasPpcsRole) {
+    res.locals.findRecallRequest = '/find-a-recall-request' // TODO !!
+    res.render('pages/startPPCS')
+  } else {
+    res.locals.searchEndpoint = '/search-by-name'
+    res.render('pages/startPage')
+  }
 }
