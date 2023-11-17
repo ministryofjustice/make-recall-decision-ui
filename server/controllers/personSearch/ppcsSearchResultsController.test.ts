92d0828d89c122e503f042eaea2847b63dc59903
robinandrewdevilliers <127398712+robinandrewdevilliers@users.noreply.github.com>
2023-11-17 09:24:38 +0000

    MRD-2127 - CaR Search Results page refinements. (#984)
    Co-authored-by: Robin de Villiers <robindevilliers@me.com>

--- server/controllers/personSearch/ppcsSearchResultsController.test.ts
+++ server/controllers/personSearch/ppcsSearchResultsController.test.ts
@@ -28,14 +28,32 @@ describe('get', () => {
     expect(res.locals.page).toEqual({ id: 'ppcsSearchResults' })
     expect(res.render).toHaveBeenCalledWith('pages/ppcsSearchResults')
 
-    expect(res.locals.results).toEqual([
-      {
-        name: 'Harry Smith',
-        crn: 'X098092',
-        dateOfBirth: '1980-05-06',
-        recommendationId: 799270715,
+    expect(res.locals.result).toEqual({
+      name: 'Harry Smith',
+      crn: 'X098092',
+      dateOfBirth: '1980-05-06',
+      recommendationId: 799270715,
+    })
+
+    expect(next).toHaveBeenCalled()
+  })
+  it('load no results', async () => {
+    ;(searchForPpcs as jest.Mock).mockResolvedValue({
+      results: [],
+    })
+
+    const res = mockRes({
+      locals: {
+        recommendation: { crn: 'X123' },
       },
-    ])
+    })
+    const next = mockNext()
+    await ppcsSearchResultsController.get(mockReq(), res, next)
+
+    expect(res.locals.page).toEqual({ id: 'ppcsSearchResults' })
+    expect(res.render).toHaveBeenCalledWith('pages/ppcsSearchResults')
+
+    expect(res.locals.result).toBeUndefined()
 
     expect(next).toHaveBeenCalled()
   })
