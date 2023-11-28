4f8d5bb25d781df4968fc113720bd00c0343101d
Darren Walker <Darren.Walker@digital.justice.gov.uk>
2023-11-28 14:09:05 +0000

    MRD-2170 Enable web instrumentation This is a bit of an experiment to see if this provides us with page load timings in App Insights.

--- server/monitoring/azureAppInsights.ts
+++ server/monitoring/azureAppInsights.ts
@@ -24,7 +24,7 @@ export function initialiseAppInsights(): void {
     // eslint-disable-next-line no-console
     console.log('Enabling azure application insights')
 
-    setup().setDistributedTracingMode(DistributedTracingModes.AI_AND_W3C).start()
+    setup().setDistributedTracingMode(DistributedTracingModes.AI_AND_W3C).enableWebInstrumentation(true).start()
   }
 }
 
