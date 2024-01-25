11e282398a2e9e67bb0758c0c7874a88d7194f7f
robinandrewdevilliers <127398712+robinandrewdevilliers@users.noreply.github.com>
2024-01-25 15:30:37 +0000

    MRD-2283 - fix to routing 2 (#1072)
    Co-authored-by: Robin de Villiers <robindevilliers@me.com>

--- server/controllers/recommendation/matchIndexOffenceController.test.ts
+++ server/controllers/recommendation/matchIndexOffenceController.test.ts
@@ -104,7 +104,7 @@ describe('post', () => {
       },
     })
 
-    expect(res.redirect).toHaveBeenCalledWith(303, `/recommendations/1/index-offence-selected`)
+    expect(res.redirect).toHaveBeenCalledWith(303, `/recommendations/1/sentence-to-commit`)
     expect(next).not.toHaveBeenCalled() // end of the line for posts.
   })
   it('post with valid data - sentences', async () => {
