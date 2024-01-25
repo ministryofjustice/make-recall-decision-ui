3cdbc0a77549755554cf730611d8e351c6f7fb1a
Bill Sclater <bill.sclater@digital.justice.gov.uk>
2024-01-25 13:28:33 +0000

    mrd-2041_edit-police-force-text-correction: correction to text around police forces as per design

--- server/controllers/recommendation/editPoliceContactController.ts
+++ server/controllers/recommendation/editPoliceContactController.ts
@@ -19,7 +19,7 @@ async function get(_: Request, res: Response, next: NextFunction) {
     }
   })
   policeForces.unshift({
-    text: 'Select police force area',
+    text: 'Select police force',
     value: '',
   })
 
