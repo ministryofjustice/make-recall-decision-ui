c135499d6c293d441689a1b8d872f1c801f997c0
Robin de Villiers <robindevilliers@me.com>
2024-01-31 11:15:31 +0000

    MRD-2286 - Your recall booking page.

--- server/controllers/recommendation/sentenceToCommitController.ts
+++ server/controllers/recommendation/sentenceToCommitController.ts
@@ -1,20 +1,28 @@
 import { NextFunction, Request, Response } from 'express'
 import { RecommendationResponse } from '../../@types/make-recall-decision-api'
 import { nextPageLinkUrl } from '../recommendations/helpers/urls'
+import { hasValue } from '../../utils/utils'
 
 async function get(_: Request, res: Response, next: NextFunction) {
   const { recommendation } = res.locals
 
-  const offence = (recommendation as RecommendationResponse).nomisIndexOffence.allOptions.find(
+  const recommendationResponse = recommendation as RecommendationResponse
+
+  const offence = recommendationResponse.nomisIndexOffence.allOptions.find(
     o => o.offenderChargeId === recommendation.nomisIndexOffence.selected
   )
+
+  const addNewSentence =
+    !hasValue(recommendationResponse.ppudOffender) ||
+    recommendationResponse.bookRecallToPpud.ppudSentenceId === 'ADD_NEW'
 
   res.locals = {
     ...res.locals,
     page: {
-      id: 'indexOffenceSelected',
+      id: 'sentenceToCommit',
     },
     offence,
+    addNewSentence,
   }
 
   res.render(`pages/recommendations/sentenceToCommit`)
