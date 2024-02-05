3a9370ca48693cfb4edcbc43519a2f1c67a6caf5
robinandrewdevilliers <127398712+robinandrewdevilliers@users.noreply.github.com>
2024-02-05 09:55:50 +0000

    MRD-2286 - removed releasing prison and offence date (#1083)
    Co-authored-by: Robin de Villiers <robindevilliers@me.com>

--- integration_tests/integration/recommendation.spec.ts
+++ integration_tests/integration/recommendation.spec.ts
@@ -2362,9 +2362,7 @@ context('Make a recommendation', () => {
 
       cy.getText('custodyType').should('contain', 'custody type')
       cy.getText('offenceDescription').should('contain', 'index offence')
-      cy.getText('offenceDate').should('contain', '17 November 2023')
       cy.getText('releaseDate').should('contain', '1 January 2025')
-      cy.getText('releasingPrison').should('contain', 'releasing prison')
       cy.getText('courtDescription').should('contain', 'court description')
       cy.getText('sentenceDate').should('contain', '16 November 2023')
       cy.getText('licenceExpiryDate').should('contain', '2 January 2025')
