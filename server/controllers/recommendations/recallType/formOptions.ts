c255b476630ae955b16fcc88f9ecb1ccc230c4be
bills7281 <104494371+bills7281@users.noreply.github.com>
2024-03-21 15:20:10 +0000

    mrd-2386_no-recall-text: updated text for no recall option (#1168)

--- server/controllers/recommendations/recallType/formOptions.ts
+++ server/controllers/recommendations/recallType/formOptions.ts
@@ -1,5 +1,5 @@
 export const recallType = [
   { value: 'STANDARD', text: 'Standard recall' },
   { value: 'FIXED_TERM', text: 'Fixed term recall' },
-  { value: 'NO_RECALL', text: 'No recall' },
+  { value: 'NO_RECALL', text: 'No recall - send a decision not to recall letter' },
 ]
