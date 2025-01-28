d63e266487420d50df22ba697bb230461b9514a3
Rick Livesey <rick.livesey@digital.justice.gov.uk>
2025-01-28 10:30:27 +0000

    Reverting management endpoints (health/info/prometheus) back to standard HTTP ports

--- scripts/start-services-for-e2e-tests.sh
+++ scripts/start-services-for-e2e-tests.sh
@@ -79,6 +79,6 @@ function wait_for {
 
 wait_for "http://localhost:9090/auth/health/ping" "${AUTH_NAME}"
 wait_for "http://localhost:3000/ping" "${UI_NAME}"
-wait_for "http://localhost:8081/health/readiness" "${API_NAME}"
+wait_for "http://localhost:8080/health/readiness" "${API_NAME}"
 
 printf "\n\nAll services are ready.\n\n"
