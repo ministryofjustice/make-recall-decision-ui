c6036b14fe1d734dd2364a8581bbcc097ab3c3be
Pablo Mascaraque Glenn <pablo.glenn@digital.justice.gov.uk>
2025-04-04 12:55:31 +0100

    MRD-2700 Implement establishment mapping logic (#1364)
    After discussions with PPCS, we are going with the following approach for the
    mapping logic:
    
        Check NOMIS for the offender's prison.
        a. If the record has a prison set, take that and do the mapping to the
        relevant PPUD value.
        b. If it's OUT , the offender is UAL and we set Not Applicable.
        c. If it's TRN , go to step 2.
        Check NOMIS for the offender's movements.
        a. If the latest one is a transfer, take the destination prison.
        b. If it's something other than a transfer, go back to step 1 to see if the
        prison value has changed to something other than TRN . If it has changed,
        go with that; otherwise leave it blank for the user to check manually.
    
    An alternative discussed to leaving the field blank at step 2.b was to just
    take the latest transfer movement and go with that, even if it isn’t the latest
    movement.
    
    An alternative discussed to step 2 was to take the latest transfer (even if it
    isn't the latest movement) and go with the destination prison there.
    
    The pitfalls of these two alternatives (i.e. that another movement exists which
    should be picked and which reflects a different establishment) are very
    unlikely to happen, and there is also a chance that NOMIS data changes after it
    is checked by CaR but before the PPCS user finishes processing the
    recommendation anyway, but ultimately we decided it was better to leave the
    field blank for the user to check manually. We expect this to happen very
    rarely (if ever), so the disruption to the ideal flow of not having to access
    NOMIS at all will very rarely (if ever) be experienced.
    
    Regarding the PPCS user being made aware of the offender being in the middle of
    a transfer: we will not be displaying this information on screen, as the only
    case where it is ever relevant to them is for figuring out the establishment,
    which we are automating here.

---
+++ server/@types/make-recall-decision-api/models/prison-api/EstablishmentMap.d.ts
@@ -0,0 +1,1 @@
+export type EstablishmentMap = Map<string, string>
---
+++ server/controllers/recommendation/ppcs/determinePpudEstablishment.ts
@@ -0,0 +1,82 @@
+import { RecommendationResponse } from '../../../@types/make-recall-decision-api'
+import { establishmentMappings, offenderMovements, searchForPrisonOffender } from '../../../data/makeDecisionApiClient'
+import { hasValue } from '../../../utils/utils'
+
+export const NOMIS_ESTABLISHMENT_OUT = 'OUT'
+export const NOMIS_ESTABLISHMENT_TRANSFER = 'TRN'
+
+export const PPUD_ESTABLISHMENT_NOT_APPLICABLE = 'Not Applicable'
+
+/**
+ * Determines the PPUD establishment corresponding to the offender's NOMIS agency ID, accounting for the cases where
+ * the offender is OUT or being transferred (TRN).
+ */
+export const determinePpudEstablishment = async (
+  recommendation: RecommendationResponse,
+  token: string
+): Promise<string> => {
+  const nomisAgencyId = recommendation.prisonOffender.agencyId
+  if (!hasValue(nomisAgencyId) || nomisAgencyId.length === 0) {
+    return ''
+  }
+  if (nomisAgencyId === NOMIS_ESTABLISHMENT_OUT) {
+    return PPUD_ESTABLISHMENT_NOT_APPLICABLE
+  }
+  if (nomisAgencyId === NOMIS_ESTABLISHMENT_TRANSFER) {
+    return getTransferAgencyId(recommendation, token)
+  }
+  return mapToPpudEstablishment(nomisAgencyId, token)
+}
+
+/**
+ * Gets the agency ID from the relevant transfer, or an empty value if not applicable
+ */
+async function getTransferAgencyId(recommendation: RecommendationResponse, token: string) {
+  let nomisAgencyId = recommendation.prisonOffender.agencyId
+
+  const latestMovement = await getLatestMovement(recommendation.personOnProbation.nomsNumber, token)
+  if (latestMovement.movementType === NOMIS_ESTABLISHMENT_TRANSFER) {
+    nomisAgencyId = latestMovement.toAgency
+    if (!hasValue(nomisAgencyId) || nomisAgencyId.length === 0) {
+      return ''
+    }
+    return mapToPpudEstablishment(nomisAgencyId, token)
+  }
+
+  // We only take the agency from the latest movement if it's a transfer. If it isn't, it means some other movement
+  // has happened since we last checked NOMIS, so we need to back to NOMIS to get the latest data
+  if (hasValue(recommendation.personOnProbation.nomsNumber)) {
+    return determineLatestOffenderPpudEstablishment(recommendation, token)
+  }
+
+  return ''
+}
+
+async function getLatestMovement(nomisId: string, token: string) {
+  const movements = await offenderMovements(token, nomisId)
+  movements.sort((movementA, movementB) => (movementA.movementDateTime < movementB.movementDateTime ? -1 : 1))
+  const latestMovement = movements[movements.length - 1]
+
+  return latestMovement
+}
+
+/**
+ * Get the most up-to-date prison offender record and map its agency ID to a PPUD establishment, ignoring transfer cases
+ */
+async function determineLatestOffenderPpudEstablishment(recommendation: RecommendationResponse, token: string) {
+  const prisonOffender = await searchForPrisonOffender(token, recommendation.personOnProbation.nomsNumber)
+  const nomisAgencyId = prisonOffender.agencyId
+  if (nomisAgencyId === NOMIS_ESTABLISHMENT_OUT) {
+    return PPUD_ESTABLISHMENT_NOT_APPLICABLE
+  }
+  if (nomisAgencyId === NOMIS_ESTABLISHMENT_TRANSFER) {
+    return ''
+  }
+  return mapToPpudEstablishment(nomisAgencyId, token)
+}
+
+async function mapToPpudEstablishment(nomisAgencyId: string, token: string) {
+  const establishmentMap = await establishmentMappings(token)
+  const ppudEstablishment = establishmentMap.get(nomisAgencyId)
+  return hasValue(ppudEstablishment) ? ppudEstablishment : ''
+}
