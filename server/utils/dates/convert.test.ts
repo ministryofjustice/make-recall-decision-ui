fc28ffc451ce3a239f9e090b374464dbfaefbe29
Pablo Glenn <pablo.glenn@digital.justice.gov.uk>
2025-06-19 10:08:50 +0100

    MRD-2687 Move date formatting file

--- server/utils/dates/convert.test.ts
+++ server/utils/dates/convert.test.ts
@@ -1,6 +1,6 @@
 import { DateTime } from 'luxon'
 import { convertGmtDatePartsToUtc, splitIsoDateToParts } from './convert'
-import { padWithZeroes } from './format'
+import { padWithZeroes } from './format/format'
 
 describe('convertGmtDatePartsToUtc', () => {
   describe('valid', () => {
