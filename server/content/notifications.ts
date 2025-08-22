b0f8f45d1c2b57098c32d46bd374bddfe64b4786
Pablo Mascaraque Glenn <pablo.glenn@digital.justice.gov.uk>
2025-08-22 17:21:48 +0100

    MRD-2795 MRD 2847 Update banner end dates (#1407)

--- server/content/notifications.ts
+++ server/content/notifications.ts
@@ -4,10 +4,10 @@ import { HMPPS_AUTH_ROLE } from '../middleware/authorisationMiddleware'
 // TODO: Update start dates to match those in MRD-2788 after this has been tested on dev
 export const notifications: Notification[] = [
   {
-    headerText: 'Out-of-hours recalls started on 1 September 2025 will need to completed by 11:59pm',
+    headerText: 'Out-of-hours recalls started on 1 September 2025 will need to be completed by 11:59pm',
     bodyContent: 'This is because the service needs to be updated in line with FTR48 rules.',
     startDate: new Date('2025-08-10T00:00:00Z'), // new Date('2025-08-19T00:00:00Z'),
-    endDate: new Date('2025-09-09T00:00:00Z'),
+    endDate: new Date('2025-09-02T16:00:00Z'),
     visibleToRoles: [HMPPS_AUTH_ROLE.RW],
   },
   {
@@ -15,7 +15,7 @@ export const notifications: Notification[] = [
     bodyContent:
       'This is because the service needs to be updated in line with FTR48 rules. You can start the recommendation again after the service resumes at 12am on 2 September.',
     startDate: new Date('2025-08-10T00:00:00Z'), // new Date('2025-08-19T00:00:00Z')
-    endDate: new Date('2025-09-09T00:00:00Z'),
+    endDate: new Date('2025-09-05T16:00:00Z'),
     visibleToRoles: [HMPPS_AUTH_ROLE.PO, HMPPS_AUTH_ROLE.SPO, HMPPS_AUTH_ROLE.ODM],
   },
 ]
