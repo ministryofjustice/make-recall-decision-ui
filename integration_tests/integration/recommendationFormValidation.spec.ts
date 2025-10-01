e234c869c255deebd55af518df32edab617f27e8
Suresh Mandalapu <231466518+sureshmoj@users.noreply.github.com>
2025-10-01 13:03:37 +0100

    remove categoryHint

--- integration_tests/integration/recommendationFormValidation.spec.ts
+++ integration_tests/integration/recommendationFormValidation.spec.ts
@@ -211,10 +211,10 @@ context('Make a recommendation - form validation', () => {
       fieldName: 'vulnerabilities',
       errorText: 'Select the vulnerabilities or needs Jane Bloggs may have, or ‘No concerns or do not know’',
     })
-    cy.selectCheckboxes('Consider if you think this recall could affect any vulnerabilities or needs Jane Bloggs may have.', [
-      'Relationship breakdown',
-      'Physical disabilities',
-    ])
+    cy.selectCheckboxes(
+      'Consider if you think this recall could affect any vulnerabilities or needs Jane Bloggs may have.',
+      ['Relationship breakdown', 'Physical disabilities']
+    )
     cy.clickButton('Continue')
     cy.assertErrorMessage({
       fieldName: 'vulnerabilitiesDetail-PHYSICAL_DISABILITIES',
