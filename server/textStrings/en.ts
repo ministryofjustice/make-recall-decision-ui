ec65658e5c6e709eb44c2a4263e812f2ece85dd6
Emily Hazlehurst <24455584+EmilyHazlehurst@users.noreply.github.com>
2025-10-09 12:51:11 +0100

    Mrd 2735 edit offence details page (#1424)
    * MRD-2735: Edit 'Offence' details
    - Add edit controller, view and tests
    - Update text strings
    - Update ppudSentenceDataGenerator to include offenceDescriptionComment
    
    * MRD-2735: Edit 'Offence' details
    - Linting
    
    * PR changes
    - Remove wrapper from textAreaId
    
    * PR changes
    - Update test to check that ppudReferenceList values are transformed correctly.
    
    * PR changes
    - Remove unnecessary nested describe blocks
    
    * PR changes
    - Adapt tests to include one for just the offenceDescription and another for both the offenceDescription and the offenceDescriptionComment having been submitted.
    
    * MRD-2704: Edit 'Date of sentence' details
    - Remove unnecessary comment

--- server/textStrings/en.ts
+++ server/textStrings/en.ts
@@ -279,6 +279,7 @@ export const strings: Record<string, Record<string, string>> = {
     editCustodyGroup: 'Is the sentence determinate or indeterminate?',
     // indeterminate sentence selection
     selectIndeterminatePpudSentence: 'Select or add a sentence for your booking',
+    editOffence: 'Edit offence',
   },
   automatedFieldValues: {
     mandatoryFTRRationale:
