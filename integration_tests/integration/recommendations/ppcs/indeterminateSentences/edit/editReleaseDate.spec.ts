55acd9acb3142c9fe3c729a973fbc79ecb362d42
Pablo Glenn <pablo.glenn@digital.justice.gov.uk>
2025-10-23 14:39:16 +0100

    Fix editReleaseDate.spec.ts
    The editReleaseDate test was failing occasionally in an irreproducible manner.
    It looks like the likely reason is that the test is assuming the local timezone
    is Europe/London, while the production code doesn't and makes sure the date is
    always printed for Europe/London. If the date randomly generated for the test
    fell on different days in Europe/London vs the local timezone, the test would
    fail. The test is here fixed to ensure it's checking for Europe/London.

--- integration_tests/integration/recommendations/ppcs/indeterminateSentences/edit/editReleaseDate.spec.ts
+++ integration_tests/integration/recommendations/ppcs/indeterminateSentences/edit/editReleaseDate.spec.ts
@@ -1,4 +1,5 @@
 import { fakerEN_GB as faker } from '@faker-js/faker'
+import { DateTime } from 'luxon'
 import { setDateInput, verifyInputs } from '../../../../../componentTests/date.tests'
 import searchMappedUserResponse from '../../../../../../api/responses/searchMappedUsers.json'
 import searchActiveUsersResponse from '../../../../../../api/responses/ppudSearchActiveUsers.json'
@@ -53,9 +54,14 @@ context('Indeterminate Sentence - Edit Release Date Page', () => {
 
       cy.get('h2').should('have.class', 'govuk-heading-m').should('contain.text', 'Currently in PPUD')
 
+      // It is possible for the test to run on a machine on a timezone other than Europe/London (which the production
+      // code uses). Therefore, we convert the date to Europe/London here to ensure the date shown on the page is as
+      // expected.
+      const ppudReleaseDateTime = DateTime.fromISO(ppudReleaseDate.toISOString(), { zone: 'Europe/London' })
+      ppudReleaseDateTime.setLocale('en-GB')
       cy.get('p.govuk-body').should(
         'contain.text',
-        `${ppudReleaseDate.getDate()} ${ppudReleaseDate.toLocaleString('default', { month: 'long' })} ${ppudReleaseDate.getFullYear()}`
+        `${ppudReleaseDateTime.day} ${ppudReleaseDateTime.monthLong} ${ppudReleaseDateTime.year}`
       )
 
       // Date input surrounds
