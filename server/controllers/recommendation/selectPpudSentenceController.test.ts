2487a687b63b62d129945ea8e9677a09a6cd6748
Pablo Glenn <pablo.glenn@digital.justice.gov.uk>
2025-12-11 12:18:55 +0000

    MRD-3014 Update selectPpudSentenceController.test.ts

--- server/controllers/recommendation/selectPpudSentenceController.test.ts
+++ server/controllers/recommendation/selectPpudSentenceController.test.ts
@@ -1,149 +1,158 @@
+import { fakerEN_GB as faker } from '@faker-js/faker'
 import { mockNext, mockReq, mockRes } from '../../middleware/testutils/mockRequestUtils'
 import { getRecommendation, updateRecommendation } from '../../data/makeDecisionApiClient'
-import recommendationApiResponse from '../../../api/responses/get-recommendation.json'
 import selectPpudSentenceController from './selectPpudSentenceController'
+import { RecommendationResponseGenerator } from '../../../data/recommendations/recommendationGenerator'
+import { ppcsPaths } from '../../routes/paths/ppcs'
 
 jest.mock('../../data/makeDecisionApiClient')
 
-describe('get', () => {
-  it('load', async () => {
-    const res = mockRes({
-      locals: {
-        recommendation: {
-          id: '123',
-          nomisIndexOffence: {
-            selected: 1,
-            allOptions: [
-              {
-                offenderChargeId: 1,
+describe('Select Determinate PPUD Sentence Controller', () => {
+  describe('get', () => {
+    const recommendation = RecommendationResponseGenerator.generate()
+    const res = mockRes({ locals: { recommendation } })
+    const next = mockNext()
+
+    describe('Non conditional logic:', () => {
+      beforeEach(async () => {
+        await selectPpudSentenceController.get(mockReq(), res, next)
+      })
+      describe('Res locals:', () => {
+        describe('Page:', () => {
+          it('- Is provided', () => expect(res.locals.page).toBeDefined())
+          it('- Correct id', () => {
+            expect(res.locals.page.id).toEqual('selectPpudSentence')
+          })
+        })
+        describe('Selected NOMIS offence:', () => {
+          it('- Is provided', () => expect(res.locals.offence).toBeDefined())
+          it('- Is correct', () => {
+            expect(res.locals.offence).toEqual(recommendation.nomisIndexOffence.allOptions[0])
+          })
+        })
+      })
+      it('- Calls render for the expected page', () =>
+        expect(res.render).toHaveBeenCalledWith(`pages/recommendations/selectPpudSentence`))
+      it('- Executes the next function', () => expect(next).toHaveBeenCalled())
+    })
+  })
+
+  describe('post', () => {
+    describe('Valid data', () => {
+      const recommendation = RecommendationResponseGenerator.generate()
+
+      const basePath = `/recommendations/123/`
+      const res = mockRes({
+        locals: {
+          urlInfo: { basePath },
+        },
+      })
+
+      const next = mockNext()
+
+      describe('Non-conditional logic:', () => {
+        const req = mockReq({
+          originalUrl: 'some-url',
+          params: { recommendationId: '123' },
+          body: { ppudSentenceId: faker.helpers.arrayElement(recommendation.ppudOffender.sentences).id },
+        })
+        beforeEach(async () => {
+          ;(getRecommendation as jest.Mock).mockResolvedValue(recommendation)
+          await selectPpudSentenceController.post(req, res, next)
+        })
+        it('- Calls next function', () => expect(next).toHaveBeenCalled())
+      })
+      describe('Conditional logic:', () => {
+        describe('Option to add new sentence to PPUD selected', () => {
+          const req = mockReq({
+            originalUrl: 'some-url',
+            params: { recommendationId: '123' },
+            body: { ppudSentenceId: 'ADD_NEW' },
+          })
+          beforeEach(async () => {
+            ;(getRecommendation as jest.Mock).mockResolvedValue(recommendation)
+            await selectPpudSentenceController.post(req, res, next)
+          })
+          it('- Updates recommendation with selection but custody type as undefined', () => {
+            expect(updateRecommendation).toHaveBeenCalledWith({
+              recommendationId: req.params.recommendationId,
+              valuesToSave: {
+                bookRecallToPpud: {
+                  ...recommendation.bookRecallToPpud,
+                  ppudSentenceId: req.body.ppudSentenceId,
+                },
               },
-            ],
+              token: res.locals.user.token,
+              featureFlags: res.locals.flags,
+            })
+          })
+          it('- Redirects to Edit Custody Type page', () => {
+            expect(res.redirect).toHaveBeenCalledWith(303, `${basePath}${ppcsPaths.editCustodyType}`)
+          })
+        })
+        describe('Existing PPUD sentence selected', () => {
+          const req = mockReq({
+            originalUrl: 'some-url',
+            params: { recommendationId: '123' },
+            body: { ppudSentenceId: faker.helpers.arrayElement(recommendation.ppudOffender.sentences).id },
+          })
+          beforeEach(async () => {
+            ;(getRecommendation as jest.Mock).mockResolvedValue(recommendation)
+            await selectPpudSentenceController.post(req, res, next)
+          })
+          it('- Updates recommendation with selection and corresponding custody type', () => {
+            const selectedSentence = recommendation.ppudOffender.sentences.find(s => s.id === req.body.ppudSentenceId)
+            expect(updateRecommendation).toHaveBeenCalledWith({
+              recommendationId: req.params.recommendationId,
+              valuesToSave: {
+                bookRecallToPpud: {
+                  ...recommendation.bookRecallToPpud,
+                  ppudSentenceId: req.body.ppudSentenceId,
+                  custodyType: selectedSentence.custodyType,
+                },
+              },
+              token: res.locals.user.token,
+              featureFlags: res.locals.flags,
+            })
+          })
+          it('- Redirects to Sentence to Commit Existing Offender page', () => {
+            expect(res.redirect).toHaveBeenCalledWith(303, `${basePath}${ppcsPaths.sentenceToCommitExistingOffender}`)
+          })
+        })
+      })
+    })
+    describe('Invalid data', () => {
+      const res = mockRes({
+        locals: {
+          urlInfo: { basePath: faker.lorem.slug() },
+        },
+      })
+      const req = mockReq({
+        originalUrl: faker.lorem.slug(),
+        params: { recommendationId: '123' },
+        body: {},
+      })
+
+      const next = mockNext()
+
+      beforeEach(async () => {
+        await selectPpudSentenceController.post(req, res, next)
+      })
+
+      it('- Sets error in session', () => {
+        expect(req.session.errors).toEqual([
+          {
+            errorId: 'noPpudSentenceSelected',
+            href: '#ppudSentenceId',
+            invalidParts: undefined,
+            name: 'ppudSentenceId',
+            text: 'Select an existing sentence or add a new one',
+            values: undefined,
           },
-        },
-      },
+        ])
+      })
+      it('- Redirects back to original page', () => expect(res.redirect).toHaveBeenCalledWith(303, req.originalUrl))
+      it('- Does not call next function', () => expect(next).not.toHaveBeenCalled())
     })
-    const next = mockNext()
-
-    await selectPpudSentenceController.get(mockReq(), res, next)
-
-    expect(res.locals.page.id).toEqual('selectPpudSentence')
-    expect(res.locals.offence).toEqual({ offenderChargeId: 1 })
-    expect(res.render).toHaveBeenCalledWith(`pages/recommendations/selectPpudSentence`)
-    expect(next).toHaveBeenCalled()
-  })
-})
-
-describe('post', () => {
-  it('select ppud sentence', async () => {
-    ;(getRecommendation as jest.Mock).mockResolvedValue({
-      ...recommendationApiResponse,
-      bookRecallToPpud: {
-        mappaLevel: 'Mappa Level 1',
-      },
-    })
-
-    const req = mockReq({
-      originalUrl: 'some-url',
-      params: { recommendationId: '123' },
-      body: { ppudSentenceId: '1234' },
-    })
-
-    const res = mockRes({
-      locals: {
-        user: { token: 'token1' },
-        recommendation: { personOnProbation: { name: 'Joe Bloggs' } },
-        urlInfo: { basePath: `/recommendations/123/` },
-      },
-    })
-
-    const next = mockNext()
-
-    await selectPpudSentenceController.post(req, res, next)
-
-    expect(updateRecommendation).toHaveBeenCalledWith({
-      featureFlags: {},
-      recommendationId: '123',
-      token: 'token',
-      valuesToSave: {
-        bookRecallToPpud: {
-          mappaLevel: 'Mappa Level 1',
-          ppudSentenceId: '1234',
-        },
-      },
-    })
-    expect(res.redirect).toHaveBeenCalledWith(303, `/recommendations/123/sentence-to-commit-existing-offender`)
-    expect(next).toHaveBeenCalled()
-  })
-  it('select add new ppud sentence', async () => {
-    ;(getRecommendation as jest.Mock).mockResolvedValue({
-      ...recommendationApiResponse,
-      bookRecallToPpud: {
-        mappaLevel: 'Mappa Level 1',
-      },
-    })
-
-    const req = mockReq({
-      originalUrl: 'some-url',
-      params: { recommendationId: '123' },
-      body: { ppudSentenceId: 'ADD_NEW' },
-    })
-
-    const res = mockRes({
-      locals: {
-        user: { token: 'token1' },
-        recommendation: { personOnProbation: { name: 'Joe Bloggs' } },
-        urlInfo: { basePath: `/recommendations/123/` },
-      },
-    })
-
-    const next = mockNext()
-
-    await selectPpudSentenceController.post(req, res, next)
-
-    expect(updateRecommendation).toHaveBeenCalledWith({
-      featureFlags: {},
-      recommendationId: '123',
-      token: 'token',
-      valuesToSave: {
-        bookRecallToPpud: {
-          mappaLevel: 'Mappa Level 1',
-          ppudSentenceId: 'ADD_NEW',
-        },
-      },
-    })
-    expect(res.redirect).toHaveBeenCalledWith(303, `/recommendations/123/custody-type`)
-    expect(next).toHaveBeenCalled()
-  })
-  it('missing ppud sentence', async () => {
-    const req = mockReq({
-      originalUrl: 'some-url',
-      params: { recommendationId: '123' },
-      body: {},
-    })
-
-    const res = mockRes({
-      locals: {
-        user: { token: 'token1' },
-        recommendation: { personOnProbation: { name: 'Joe Bloggs' } },
-        urlInfo: { basePath: `/recommendations/123/` },
-      },
-    })
-
-    const next = mockNext()
-
-    await selectPpudSentenceController.post(req, res, next)
-    expect(res.redirect).toHaveBeenCalledWith(303, `some-url`)
-
-    expect(req.session.errors).toEqual([
-      {
-        errorId: 'noPpudSentenceSelected',
-        href: '#ppudSentenceId',
-        invalidParts: undefined,
-        name: 'ppudSentenceId',
-        text: 'Select an existing sentence or add a new one',
-        values: undefined,
-      },
-    ])
-    expect(next).not.toHaveBeenCalled()
   })
 })
