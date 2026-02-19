b77c83514c346e93ee232cd30864c584571747af
Pablo Glenn <pablo.glenn@digital.justice.gov.uk>
2026-02-19 15:54:23 +0000

    MRD-3089 Test refactor - part 2

--- server/controllers/recommendation/triggerLeadingToRecallController.test.ts
+++ server/controllers/recommendation/triggerLeadingToRecallController.test.ts
@@ -1,125 +1,178 @@
+import { Response } from 'express'
+import { fakerEN_GB as faker } from '@faker-js/faker'
 import triggerLeadingToRecallController from './triggerLeadingToRecallController'
 import { mockNext, mockReq, mockRes } from '../../middleware/testutils/mockRequestUtils'
 import { updateRecommendation } from '../../data/makeDecisionApiClient'
 import { RecommendationResponseGenerator } from '../../../data/recommendations/recommendationGenerator'
 import { ppPaths } from '../../routes/paths/pp'
+import { strings } from '../../textStrings/en'
 
 jest.mock('../../data/makeDecisionApiClient')
 
-describe('get', () => {
-  it('load with no data', async () => {
-    const res = mockRes({
-      locals: {
-        recommendation: RecommendationResponseGenerator.generate({ triggerLeadingToRecall: false }),
-      },
+describe('Trigger Leading to Recall Controller', () => {
+  describe('get', () => {
+    describe('Non-conditional logic:', () => {
+      const recommendation = RecommendationResponseGenerator.generate()
+      const next = mockNext()
+      let res: Response
+      beforeEach(() => {
+        res = mockRes({
+          locals: {
+            recommendation,
+          },
+        })
+        triggerLeadingToRecallController.get(mockReq(), res, next)
+      })
+
+      describe('Res locals', () => {
+        describe('Page Data is provided:', () => {
+          it('Page ID', () => expect(res.locals.page).toEqual({ id: 'triggerLeadingToRecall' }))
+          it('Recommendation', () => expect(res.locals.recommendation).toEqual(recommendation))
+        })
+      })
+
+      it('- Calls render for the expected page', async () =>
+        expect(res.render).toHaveBeenCalledWith(`pages/recommendations/triggerLeadingToRecall`))
+
+      it('- Executes the next function', async () => expect(next).toHaveBeenCalled())
     })
-    const next = mockNext()
-    triggerLeadingToRecallController.get(mockReq(), res, next)
 
-    expect(res.locals.page).toEqual({ id: 'triggerLeadingToRecall' })
-    expect(res.locals.inputDisplayValues.value).not.toBeDefined()
-    expect(res.render).toHaveBeenCalledWith('pages/recommendations/triggerLeadingToRecall')
+    describe('Conditional logic:', () => {
+      describe('No errors defined', () => {
+        describe('No previous trigger leading to recall recorded:', () => {
+          let res: Response
+          const recommendation = RecommendationResponseGenerator.generate({ triggerLeadingToRecall: false })
+          beforeEach(() => {
+            res = mockRes({
+              locals: {
+                recommendation,
+              },
+            })
+            triggerLeadingToRecallController.get(mockReq(), res, mockNext())
+          })
+          describe('Res locals', () => {
+            describe('Page Data is provided:', () => {
+              it('No trigger value', () => expect(res.locals.inputDisplayValues.value).not.toBeDefined())
+              it('No errors', () => expect(res.locals.inputDisplayValues.errors).not.toBeDefined())
+            })
+          })
+        })
 
-    expect(next).toHaveBeenCalled()
+        describe('Previous trigger leading to recall recorded:', () => {
+          let res: Response
+          const recommendation = RecommendationResponseGenerator.generate({ triggerLeadingToRecall: true })
+          beforeEach(() => {
+            res = mockRes({
+              locals: {
+                recommendation,
+              },
+            })
+            const next = mockNext()
+            triggerLeadingToRecallController.get(mockReq(), res, next)
+          })
+          describe('Res locals', () => {
+            describe('Page Data is provided:', () => {
+              it('Trigger value', () =>
+                expect(res.locals.inputDisplayValues.value).toEqual(recommendation.triggerLeadingToRecall))
+              it('No errors', () => expect(res.locals.inputDisplayValues.errors).not.toBeDefined())
+            })
+          })
+        })
+      })
+
+      describe('Errors defined', () => {
+        const error = faker.lorem.sentence()
+        let res: Response
+        beforeEach(() => {
+          res = mockRes({
+            locals: {
+              errors: { triggerLeadingToRecall: error },
+              recommendation: RecommendationResponseGenerator.generate(),
+            },
+          })
+          triggerLeadingToRecallController.get(mockReq(), res, mockNext())
+        })
+        describe('Res locals', () => {
+          describe('Page Data is provided:', () => {
+            it('No trigger value', () => expect(res.locals.inputDisplayValues.value).toEqual(''))
+            it('Errors for triggerLeadingToRecall', () =>
+              expect(res.locals.inputDisplayValues.errors.triggerLeadingToRecall).toEqual(error))
+          })
+        })
+      })
+    })
   })
 
-  it('load with existing data', async () => {
-    const recommendation = RecommendationResponseGenerator.generate({ triggerLeadingToRecall: true })
-    const res = mockRes({
-      locals: {
-        recommendation,
-      },
+  describe('post', () => {
+    describe('Valid trigger text provided:', () => {
+      const recommendationId = faker.number.int().toString()
+      const triggerLeadingToRecall = faker.lorem.paragraph()
+      const req = mockReq({
+        params: { recommendationId },
+        body: { triggerLeadingToRecall },
+      })
+
+      const basePath = faker.internet.url()
+      const next = mockNext()
+      let res: Response
+      beforeEach(async () => {
+        res = mockRes({
+          token: 'token1',
+          locals: {
+            urlInfo: { basePath },
+          },
+        })
+        ;(updateRecommendation as jest.Mock).mockResolvedValue({})
+
+        await triggerLeadingToRecallController.post(req, res, next)
+      })
+
+      it('No errors logged', () => expect(req.session.errors).toBeUndefined())
+      it('Recommendation updated', () => {
+        expect(updateRecommendation).toHaveBeenCalledWith({
+          recommendationId,
+          token: res.locals.user.token,
+          valuesToSave: {
+            triggerLeadingToRecall,
+          },
+          featureFlags: {},
+        })
+      })
+      it('Redirected to task-list-consider-recall page', () =>
+        expect(res.redirect).toHaveBeenCalledWith(303, `${basePath}${ppPaths.taskListConsiderRecall}`))
+      expect(next).not.toHaveBeenCalled() // end of the line for posts.
     })
 
-    triggerLeadingToRecallController.get(mockReq(), res, mockNext())
-    expect(res.locals.inputDisplayValues.value).toEqual(recommendation.triggerLeadingToRecall)
-  })
+    describe('Invalid trigger text provided', () => {
+      const recommendationId = faker.number.int().toString()
+      const originalUrl = faker.internet.url()
+      const req = mockReq({
+        params: { recommendationId },
+        body: { triggerLeadingToRecall: '' },
+        originalUrl,
+      })
 
-  it('initial load with error data', async () => {
-    const recommendation = RecommendationResponseGenerator.generate({ triggerLeadingToRecall: true })
-    const res = mockRes({
-      locals: {
-        errors: { triggerLeadingToRecall: { text: 'val' } },
-        recommendation,
-      },
+      const next = mockNext()
+      let res: Response
+      beforeEach(async () => {
+        res = mockRes()
+        await triggerLeadingToRecallController.post(req, res, next)
+      })
+
+      it('Error for missing trigger added to session', () => {
+        expect(req.session.errors).toEqual([
+          {
+            errorId: 'missingTriggerLeadingToRecall',
+            href: '#triggerLeadingToRecall',
+            invalidParts: undefined,
+            name: 'triggerLeadingToRecall',
+            text: strings.errors.missingTriggerLeadingToRecall,
+            values: undefined,
+          },
+        ])
+      })
+      it('Redirected to same page', () => expect(res.redirect).toHaveBeenCalledWith(303, req.originalUrl))
+      it('Recommendation is not updated', () => expect(updateRecommendation).not.toHaveBeenCalled())
     })
-
-    triggerLeadingToRecallController.get(mockReq(), res, mockNext())
-    expect(res.locals.inputDisplayValues.value).toEqual('')
-    expect(res.locals.inputDisplayValues.errors).toEqual({ triggerLeadingToRecall: { text: 'val' } })
-  })
-})
-
-describe('post', () => {
-  it('post with valid data', async () => {
-    ;(updateRecommendation as jest.Mock).mockResolvedValue({})
-
-    const recommendationId = '123'
-    const triggerLeadingToRecall = 'some value'
-    const basePath = `/recommendations/${recommendationId}/`
-    const req = mockReq({
-      params: { recommendationId },
-      body: { triggerLeadingToRecall },
-    })
-
-    const res = mockRes({
-      token: 'token1',
-      locals: {
-        recommendation: { personOnProbation: { name: 'Joe Bloggs' } },
-        urlInfo: { basePath },
-      },
-    })
-    const next = mockNext()
-
-    await triggerLeadingToRecallController.post(req, res, next)
-
-    expect(updateRecommendation).toHaveBeenCalledWith({
-      recommendationId,
-      token: res.locals.user.token,
-      valuesToSave: {
-        triggerLeadingToRecall,
-      },
-      featureFlags: {},
-    })
-
-    expect(res.redirect).toHaveBeenCalledWith(
-      303,
-      `/recommendations/${recommendationId}/${ppPaths.taskListConsiderRecall}`
-    )
-    expect(next).not.toHaveBeenCalled() // end of the line for posts.
-  })
-
-  it('post with invalid data', async () => {
-    ;(updateRecommendation as jest.Mock).mockResolvedValue({})
-
-    const recommendationId = '123'
-    const req = mockReq({
-      originalUrl: 'some-url',
-      params: { recommendationId },
-      body: { triggerLeadingToRecall: '' },
-    })
-
-    const res = mockRes({
-      locals: {
-        user: { token: 'token1' },
-        recommendation: { personOnProbation: { name: 'Joe Bloggs' } },
-        urlInfo: { basePath: `/recommendations/${recommendationId}/` },
-      },
-    })
-
-    await triggerLeadingToRecallController.post(req, res, mockNext())
-
-    expect(updateRecommendation).not.toHaveBeenCalled()
-    expect(req.session.errors).toEqual([
-      {
-        errorId: 'missingTriggerLeadingToRecall',
-        href: '#triggerLeadingToRecall',
-        invalidParts: undefined,
-        name: 'triggerLeadingToRecall',
-        text: 'Explain what has made you consider recalling {{ fullName }}',
-        values: undefined,
-      },
-    ])
-    expect(res.redirect).toHaveBeenCalledWith(303, req.originalUrl)
   })
 })
