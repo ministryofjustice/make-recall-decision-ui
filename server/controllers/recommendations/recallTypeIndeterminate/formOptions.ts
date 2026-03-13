18af59b0617b0f3d22491b1a3e4f499a517786e2
fincardy-moj <fin.cardy@justice.gov.uk>
2026-03-13 13:52:10 +0000

    MRD-3078 update for indeterminate and extended sentences

--- server/controllers/recommendations/recallTypeIndeterminate/formOptions.ts
+++ server/controllers/recommendations/recallTypeIndeterminate/formOptions.ts
@@ -3,6 +3,11 @@ export const recallTypeIndeterminate = [
   { value: 'NO_RECALL', text: 'No recall' },
 ]
 
+export const recallTypeIndeterminateFTR56 = [
+  { value: 'EMERGENCY', text: 'Emergency standard recall' },
+  { value: 'NO_RECALL', text: 'No recall - send a decision not to recall letter' },
+]
+
 // valid values to send to MRD API for the recallType property
 export const recallTypeIndeterminateApi = [
   { value: 'STANDARD', text: 'Standard recall' },
--- server/controllers/recommendations/recallTypeIndeterminate/formValidator.ts
+++ server/controllers/recommendations/recallTypeIndeterminate/formValidator.ts
@@ -4,7 +4,11 @@ import strings from '../../../textStrings/en'
 import EVENTS from '../../../utils/constants'
 import { FormValidatorArgs, FormValidatorReturn } from '../../../@types/pagesForms'
 
-const validateRecallTypeIndeterminate = async ({ requestBody, urlInfo }: FormValidatorArgs): FormValidatorReturn => {
+const validateRecallTypeIndeterminate = async ({
+  requestBody,
+  urlInfo,
+  flagFTR56Enabled = false,
+}: FormValidatorArgs & { flagFTR56Enabled?: boolean }): FormValidatorReturn => {
   const { recallType } = requestBody
   const invalidRecallTypeIndeterminate = !isValueValid(recallType as string, 'recallTypeIndeterminate')
   const hasError = !recallType || invalidRecallTypeIndeterminate
@@ -12,7 +16,7 @@ const validateRecallTypeIndeterminate = async ({ requestBody, urlInfo }: FormVal
     const errors = []
     let errorId
     if (!recallType || invalidRecallTypeIndeterminate) {
-      errorId = 'noRecallTypeIndeterminateSelected'
+      errorId = flagFTR56Enabled ? 'noRecallTypeIndeterminateSelectedFTR56' : 'noRecallTypeIndeterminateSelected'
       errors.push(
         makeErrorObject({
           id: 'recallType',
