def69ecf742f7546857cd71f29749eede6f8c7d3
fincardy-moj <fin.cardy@justice.gov.uk>
2026-03-16 15:45:56 +0000

    Mrd 3101 display banner for ftr56 (#1522)
    * MRD-3101 display banner for FTR56
    
    * MRD-3101 remove PPCS from roles
    
    * MRD-3101 fix the order of the banners

--- server/content/notifications.ts
+++ server/content/notifications.ts
@@ -18,20 +18,20 @@ const notifications: Notification[] = [
     visibleToRoles: [HMPPS_AUTH_ROLE.PO, HMPPS_AUTH_ROLE.SPO, HMPPS_AUTH_ROLE.ODM],
   },
   {
-    headerText: 'Any Part A not downloaded by 11.59pm on 30 March 2026 will be deleted',
-    bodyContent:
-      'This is because the service needs to be updated in line with new FTR56 rules and changes to the Part A document. You can start the recommendation again after the service resumes at 12am on 31 March 2026.',
-    startDate: new Date('2025-03-17T00:00:00Z'),
-    endDate: new Date('2026-03-31T00:00:00Z'),
-    visibleToRoles: [HMPPS_AUTH_ROLE.PO, HMPPS_AUTH_ROLE.SPO],
-  },
-  {
     headerText: 'Out-of-hours recalls started on 30 March 2026 will need to be completed by 11:59pm',
     bodyContent:
       'This is because the service needs to be updated in line with new FTR56 rules and changes to the Part A document.',
     startDate: new Date('2025-03-17T00:00:00Z'),
     endDate: new Date('2026-03-31T00:00:00Z'),
     visibleToRoles: [HMPPS_AUTH_ROLE.RW, HMPPS_AUTH_ROLE.ODM],
+  },
+  {
+    headerText: 'Any Part A not downloaded by 11.59pm on 30 March 2026 will be deleted',
+    bodyContent:
+      'This is because the service needs to be updated in line with new FTR56 rules and changes to the Part A document. You can start the recommendation again after the service resumes at 12am on 31 March 2026.',
+    startDate: new Date('2025-03-17T00:00:00Z'),
+    endDate: new Date('2026-03-31T00:00:00Z'),
+    visibleToRoles: [HMPPS_AUTH_ROLE.PO, HMPPS_AUTH_ROLE.SPO],
   },
 ]
 
