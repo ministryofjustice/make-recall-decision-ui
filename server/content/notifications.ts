d1e4ec069b041bd550c83ff174734820e9fa9208
fincardy-moj <fin.cardy@justice.gov.uk>
2026-03-17 10:59:48 +0000

    MRD-3101 update banner title and banner date (#1523)
    * MRD-3101 update banner title and banner date
    
    * MRD-3101 revert alert title due to accessbility issues

--- server/content/notifications.ts
+++ server/content/notifications.ts
@@ -21,7 +21,7 @@ const notifications: Notification[] = [
     headerText: 'Out-of-hours recalls started on 30 March 2026 will need to be completed by 11:59pm',
     bodyContent:
       'This is because the service needs to be updated in line with new FTR56 rules and changes to the Part A document.',
-    startDate: new Date('2025-03-17T00:00:00Z'),
+    startDate: new Date('2026-03-17T00:00:00Z'),
     endDate: new Date('2026-03-31T00:00:00Z'),
     visibleToRoles: [HMPPS_AUTH_ROLE.RW, HMPPS_AUTH_ROLE.ODM],
   },
@@ -29,7 +29,7 @@ const notifications: Notification[] = [
     headerText: 'Any Part A not downloaded by 11.59pm on 30 March 2026 will be deleted',
     bodyContent:
       'This is because the service needs to be updated in line with new FTR56 rules and changes to the Part A document. You can start the recommendation again after the service resumes at 12am on 31 March 2026.',
-    startDate: new Date('2025-03-17T00:00:00Z'),
+    startDate: new Date('2026-03-17T00:00:00Z'),
     endDate: new Date('2026-03-31T00:00:00Z'),
     visibleToRoles: [HMPPS_AUTH_ROLE.PO, HMPPS_AUTH_ROLE.SPO],
   },
