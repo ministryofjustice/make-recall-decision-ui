80c664bf35614fbffcddec0756af0f642041f6cc
Pablo Mascaraque Glenn <pablo.glenn@digital.justice.gov.uk>
2026-03-26 18:07:10 +0000

    MRD-3069 Fix custody type integration test (#1536)
    The custody type generator would always set a selected custody type, even when
    undefined was passed through. This was causing an integration test to fail
    intermitently, as it expected a field to be hidden, but for one possible value
    of the custody type it was shown. This has been fixed so that no custody type
    is selected in the case we need.

--- data/recommendations/custodyStatusGenerator.ts
+++ data/recommendations/custodyStatusGenerator.ts
@@ -1,10 +1,10 @@
 import { fakerEN_GB as faker } from '@faker-js/faker'
 import { CustodyStatus } from '../../server/@types/make-recall-decision-api/models/CustodyStatus'
 import { TextValueOptionGenerator, TextValueOptionOptions } from '../common/textValueOptionGenerator'
-import { DataGenerator } from '../@generators/dataGenerators'
+import { DataGenerator, NoneOrOption } from '../@generators/dataGenerators'
 
 export type CustodyStatusOptions = {
-  selected?: CustodyStatus.selected
+  selected?: NoneOrOption<CustodyStatus.selected>
   details?: string
   allOptions?: Array<TextValueOptionOptions>
 }
@@ -19,7 +19,8 @@ export const CustodyStatusGenerator: DataGenerator<CustodyStatus, CustodyStatusO
       }
     }
     return {
-      selected: options.selected ?? faker.helpers.enumValue(CustodyStatus.selected),
+      selected:
+        options.selected === 'none' ? undefined : (options.selected ?? faker.helpers.enumValue(CustodyStatus.selected)),
       details: options.details ?? faker.location.streetAddress(),
       allOptions: (options.allOptions && TextValueOptionGenerator.generateSeries(options.allOptions)) ?? [],
     }
