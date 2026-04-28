7601bd60712d75cbe88600f01717f91190baab1c
Pablo Glenn <pablo.glenn@digital.justice.gov.uk>
2026-04-28 14:59:03 +0100

    MRD-2780 Migrate to GitHub Actions
    This set of changes migrates the repo away from CircleCI and onto GitHub
    Actions.
    
    Notable details of these changes include:
    - The integration and e2e local tests are run in parallel jobs, for which
      cypress-split needed to be installed and configured (changes to package.json
      and cypress.config.ts). See the hmpps-assess-risks-and-needs-github-actions
      repo for details on how the splitting and parallelisation is done. This is
      also being done in the e2e repo.
    - The contents of the start-services-for-e2e-tests.sh script have been
      simplified by copying over the contents of the start-services-integration.sh
      script that existed in the e2e test repo. This is so that the workflow in
      this and the e2e test and API pipelines are as similar as possible.
      The e2e script and the one in this repo were very similar, with this one
      accepting a couple of flag options. Those options aren't needed anymore (the
      auth images have multi-architecture support, removing the need for the -a
      flag, and we're OK with always pulling, removing the need for the -p flag).
    - The snyk security scan has been added. This replaces the trivy scan, which
      had to be removed a few weeks back due to security issues in trivy. The snyk
      one has been added using the snyk-add.sh script in the hmpps-github-actions
      repo.

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -6,6 +6,7 @@ export default configureAllowedScripts({
     'node_modules/@parcel/watcher@2.5.6': 'ALLOW',
     'node_modules/cypress@15.13.1': 'ALLOW',
     'node_modules/dtrace-provider@0.8.8': 'ALLOW',
+    'node_modules/esbuild@0.27.7': 'ALLOW',
     'node_modules/fsevents@2.3.3': 'ALLOW',
     // Native solution to quickly resolve module paths, used by jest and eslint
     'node_modules/unrs-resolver@1.11.1': 'ALLOW',
