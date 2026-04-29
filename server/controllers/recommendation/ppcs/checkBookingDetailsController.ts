c8f54e90c4155d366ebc892360fce155db059e06
sureshmoj <suresh.mandalapu@justice.gov.uk>
2026-04-29 16:36:16 +0100

    MRD-3042: Recall received date/time fields to reflect when PPCS received the PartA (#1589)

--- server/controllers/recommendation/ppcs/checkBookingDetailsController.ts
+++ server/controllers/recommendation/ppcs/checkBookingDetailsController.ts
@@ -102,10 +102,6 @@ async function get(_: Request, res: Response, next: NextFunction) {
       currentEstablishment = await determinePpudEstablishment(recommendation, token)
     }
 
-    const sentToPpcs = (statuses as RecommendationStatusResponse[])
-      .filter(s => s.active)
-      .find(s => s.name === STATUSES.SENT_TO_PPCS)
-
     valuesToSave.bookRecallToPpud = {
       firstNames: `${firstName} ${middleName}`.trim(),
       lastName,
@@ -115,7 +111,9 @@ async function get(_: Request, res: Response, next: NextFunction) {
       // When a recall is OOH, the recall received and recall decision date/time need to
       // match, so we use the decision date provided by the PP during the recommendation process
       // see: https://dsdmoj.atlassian.net/browse/MRD-3015
-      receivedDateTime: isOutOfHoursRecall ? decisionDateTime : sentToPpcs?.created,
+      // if its non OOH, force ppcs users to enter the date and time they received the recall,
+      // even if it is the same as the decision date/time, see: MRD-3042
+      receivedDateTime: isOutOfHoursRecall ? decisionDateTime : null,
       currentEstablishment,
     } as BookRecallToPpud
     recommendation.bookRecallToPpud = valuesToSave.bookRecallToPpud
@@ -253,6 +251,7 @@ async function post(req: Request, res: Response, next: NextFunction) {
   }
 
   validateBookRecallToPpudField(bookRecallToPpud, 'currentEstablishment', 'missingCurrentEstablishment', errors)
+  validateBookRecallToPpudField(bookRecallToPpud, 'receivedDateTime', 'missingReceivedDateTime', errors)
   validateBookRecallToPpudField(bookRecallToPpud, 'probationArea', 'missingProbationArea', errors)
   validateBookRecallToPpudField(bookRecallToPpud, 'policeForce', 'missingPoliceForce', errors)
   validateBookRecallToPpudField(bookRecallToPpud, 'releasingPrison', 'missingReleasingPrison', errors)
