ecf29eae380564aadb94ed36de97a6b2bdeb5d9e
Bhanu Yandra <bhanu.yandra@justice.gov.uk>
2026-04-29 18:08:29 +0100

    MRD-2716: integration tests review comments answers

--- integration_tests/integration/recommendations/partA/selectPpudSentence.spec.ts
+++ integration_tests/integration/recommendations/partA/selectPpudSentence.spec.ts
@@ -1,4 +1,5 @@
 import { fakerEN_GB as faker } from '@faker-js/faker'
+import { testForErrorPageTitle } from '../../../componentTests/errors.tests'
 import { RecommendationResponseGenerator } from '../../../../data/recommendations/recommendationGenerator'
 import CUSTODY_GROUP from '../../../../server/@types/make-recall-decision-api/models/ppud/CustodyGroup'
 import RECOMMENDATION_STATUS from '../../../../server/middleware/recommendationStatus'
@@ -42,6 +43,40 @@ context('Select PPUD Sentence', () => {
     },
   })
 
+  const recommendationWithNoSelectedSentence = RecommendationResponseGenerator.generate({
+    nomisIndexOffence: {
+      selectedIndex: 1,
+    },
+    id: recommendationId,
+    bookRecallToPpud: {
+      firstName: 'John',
+      lastName: 'Doe',
+      custodyGroup: CUSTODY_GROUP.DETERMINATE,
+      ppudSentenceId: 'NEW_PUD',
+    },
+    ppudOffender: {
+      sentences: [
+        {
+          id: '1',
+          custodyType: 'EDS',
+          offence: { indexOffence: 'EDS offence' },
+          dateOfSentence: new Date('2020-01-01'),
+        },
+        {
+          id: '2',
+          custodyType: 'DPP',
+          offence: { indexOffence: 'DPP offence' },
+          dateOfSentence: new Date('2020-01-01'),
+        },
+        {
+          id: '3',
+          custodyType: 'Determinate',
+          offence: { indexOffence: 'Determinate offence' },
+          dateOfSentence: new Date('2020-01-01'),
+        },
+      ],
+    },
+  })
   const testPageUrl = `/recommendations/${recommendationId}/select-ppud-sentence`
 
   beforeEach(() => {
@@ -51,6 +86,13 @@ context('Select PPUD Sentence', () => {
   })
 
   describe('Filtering', () => {
+    beforeEach(() => {
+      cy.task('getStatuses', {
+        statusCode: 200,
+        response: [{ name: RECOMMENDATION_STATUS.SENT_TO_PPCS, active: true }],
+      })
+      cy.task('getRecommendation', { statusCode: 200, response: recommendation })
+    })
     it('should only show determinate sentences and not indeterminate ones', () => {
       cy.visit(testPageUrl)
 
@@ -75,6 +117,34 @@ context('Select PPUD Sentence', () => {
       cy.contains('.govuk-radios__item', 'EDS').find('input[type="radio"]').should('be.checked')
 
       cy.contains('.govuk-radios__item', 'Determinate').find('input[type="radio"]').should('not.be.checked')
+    })
+    it('should not auto select when no determinate sentences are previously selected ', () => {
+      cy.task('getRecommendation', { statusCode: 200, response: recommendationWithNoSelectedSentence })
+      cy.visit(testPageUrl)
+
+      cy.get('.govuk-radios__input').should('exist')
+
+      cy.contains('.govuk-radios__item', 'EDS').find('input[type="radio"]').should('not.be.checked')
+
+      cy.contains('.govuk-radios__item', 'Determinate').find('input[type="radio"]').should('not.be.checked')
+    })
+  })
+
+  describe('Error message display', () => {
+    beforeEach(() => {
+      cy.task('getStatuses', {
+        statusCode: 200,
+        response: [{ name: RECOMMENDATION_STATUS.SENT_TO_PPCS, active: true }],
+      })
+      cy.task('getRecommendation', { statusCode: 200, response: recommendationWithNoSelectedSentence })
+    })
+    it('Displays error message when no sentence is selected', () => {
+      cy.visit(testPageUrl)
+
+      cy.get('button.govuk-button').click()
+
+      testForErrorPageTitle()
+      // No Summary errors check required as this page is not using summary component.
     })
   })
 })
