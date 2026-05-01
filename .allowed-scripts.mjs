9e563ff5d5322014872ba1f48d5a0bcd9d1fb96e
fincardy-moj <fin.cardy@justice.gov.uk>
2026-05-01 13:51:10 +0100

    update allowed scripts

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -6,9 +6,8 @@ export default configureAllowedScripts({
     'node_modules/@parcel/watcher@2.5.6': 'ALLOW',
     'node_modules/cypress@15.13.1': 'ALLOW',
     'node_modules/dtrace-provider@0.8.8': 'ALLOW',
-    'node_modules/fsevents@2.3.3': 'ALLOW',
     // Native solution to quickly resolve module paths, used by jest and eslint
     'node_modules/unrs-resolver@1.11.1': 'ALLOW',
-    'node_modules/esbuild@0.27.4': 'ALLOW',
+    'node_modules/esbuild@0.27.7': 'ALLOW',
   },
 })
