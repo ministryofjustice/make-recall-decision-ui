3e92905027c39dcd5ec1d78d52e7429f5f40a0f7
fincardy-moj <fin.cardy@justice.gov.uk>
2026-05-12 16:19:56 +0100

    MRD-2994 fix middleware bug

--- server/middleware/asyncMiddleware.ts
+++ server/middleware/asyncMiddleware.ts
@@ -3,9 +3,9 @@ import type { Request, Response, NextFunction, RequestHandler } from 'express'
 export default function asyncMiddleware(fn: RequestHandler) {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
-      return fn(req, res, next)
+      await fn(req, res, next)
     } catch (err) {
-      return next(err)
+      next(err)
     }
   }
 }
