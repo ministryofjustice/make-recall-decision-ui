1c0cb156cf6d88e508c5d77d60172b491976cf7a
fincardy-moj <fin.cardy@justice.gov.uk>
2026-07-09 16:46:13 +0100

    MRD-3241 remove unused generator option

--- data/recommendations/isRecalledOnNewChargedOrConvictedOffenceGenerator.ts
+++ data/recommendations/isRecalledOnNewChargedOrConvictedOffenceGenerator.ts
@@ -1,11 +1,9 @@
 import { fakerEN_GB as faker } from '@faker-js/faker'
 import { IsRecalledOnNewChargedOrConvictedOffence } from '../../server/@types/make-recall-decision-api/models/IsRecalledOnNewChargedOrConvictedOffence'
 import { DataGenerator, NoneOrOption } from '../@generators/dataGenerators'
-import { TextValueOptionOptions } from '../common/textValueOptionGenerator'
 
 export type IsRecalledOnNewChargedOrConvictedOffenceOptions = {
   selected?: NoneOrOption<IsRecalledOnNewChargedOrConvictedOffence.selected>
-  allOptions?: Array<TextValueOptionOptions>
 }
 
 export const isRecalledOnNewChargedOrConvictedOffenceGenerator: DataGenerator<
