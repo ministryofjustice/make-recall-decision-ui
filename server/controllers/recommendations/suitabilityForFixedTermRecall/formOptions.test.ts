352bab4ded7a7da10cdcfab5817374254bb479d0
Pablo Glenn <pablo.glenn@digital.justice.gov.uk>
2026-07-10 11:06:36 +0100

    MRD-3241 Remove isChargedWithOffence question

--- server/controllers/recommendations/suitabilityForFixedTermRecall/formOptions.test.ts
+++ server/controllers/recommendations/suitabilityForFixedTermRecall/formOptions.test.ts
@@ -2,33 +2,46 @@ import { SentenceGroup } from '../sentenceInformation/formOptions'
 import suitabilityFormOptions from './formOptions'
 
 describe('suitability for fixed term recall form options', () => {
-  it('should return the correct options for ADULT_SDS', () => {
-    const result = suitabilityFormOptions('Test Test', SentenceGroup.ADULT_SDS)
+  ;[true, false].forEach(ftr56SentenceConvictionFeatureFlag => {
+    describe(`with ftr56SentenceConvictionFeatureFlag ${ftr56SentenceConvictionFeatureFlag ? 'enabled' : 'disabled'}`, () => {
+      it('should return the correct options for ADULT_SDS', () => {
+        const result = suitabilityFormOptions('Test Test', SentenceGroup.ADULT_SDS, ftr56SentenceConvictionFeatureFlag)
 
-    expect(result).toEqual({
-      isAtRiskOfInvolvedInForeignPowerThreat: {
-        label: 'Is Test Test considered to be a person at risk of being involved in foreign power threat activity?',
-      },
-      isChargedWithOffence: { label: 'Is Test Test being recalled because of being charged with an offence?' },
-      isServingDCRSentence: { label: 'Is Test Test serving a Discretionary conditional release (DCR) sentence?' },
-      isServingSOPCSentence: { label: 'Is Test Test serving a Sentence for offenders of particular concern (SOPC)?' },
-      isServingTerroristOrNationalSecurityOffence: {
-        label: 'Is Test Test serving a sentence for a terrorist or national security offence?',
-      },
-      wasReferredToParoleBoard244ZB: {
-        label: 'Was Test Test referred to the Parole Board under section 244ZB (power to detain) on this sentence?',
-      },
-      wasRepatriatedForMurder: { label: 'Has Test Test been repatriated to the UK following a sentence for murder?' },
-    })
-  })
+        const expectedOptions = {
+          isAtRiskOfInvolvedInForeignPowerThreat: {
+            label: 'Is Test Test considered to be a person at risk of being involved in foreign power threat activity?',
+          },
+          isChargedWithOffence: { label: 'Is Test Test being recalled because of being charged with an offence?' },
+          isServingDCRSentence: { label: 'Is Test Test serving a Discretionary conditional release (DCR) sentence?' },
+          isServingSOPCSentence: {
+            label: 'Is Test Test serving a Sentence for offenders of particular concern (SOPC)?',
+          },
+          isServingTerroristOrNationalSecurityOffence: {
+            label: 'Is Test Test serving a sentence for a terrorist or national security offence?',
+          },
+          wasReferredToParoleBoard244ZB: {
+            label: 'Was Test Test referred to the Parole Board under section 244ZB (power to detain) on this sentence?',
+          },
+          wasRepatriatedForMurder: {
+            label: 'Has Test Test been repatriated to the UK following a sentence for murder?',
+          },
+        }
+        if (ftr56SentenceConvictionFeatureFlag) {
+          delete expectedOptions.isChargedWithOffence
+        }
 
-  it('should return the correct options for YOUTH_SDS', () => {
-    const result = suitabilityFormOptions('Test Test', SentenceGroup.YOUTH_SDS)
-    expect(result).toEqual({
-      isYouthChargedWithSeriousOffence: {
-        label: 'Is Test Test being recalled because of being charged with a serious offence?',
-      },
-      isYouthSentenceOver12Months: { label: "Is Test Test's sentence 12 months or over?" },
+        expect(result).toEqual(expectedOptions)
+      })
+
+      it('should return the correct options for YOUTH_SDS', () => {
+        const result = suitabilityFormOptions('Test Test', SentenceGroup.YOUTH_SDS, ftr56SentenceConvictionFeatureFlag)
+        expect(result).toEqual({
+          isYouthChargedWithSeriousOffence: {
+            label: 'Is Test Test being recalled because of being charged with a serious offence?',
+          },
+          isYouthSentenceOver12Months: { label: "Is Test Test's sentence 12 months or over?" },
+        })
+      })
     })
   })
 })
--- server/controllers/recommendations/suitabilityForFixedTermRecall/formOptions.ts
+++ server/controllers/recommendations/suitabilityForFixedTermRecall/formOptions.ts
@@ -1,7 +1,29 @@
 import { SentenceGroup } from '../sentenceInformation/formOptions'
 
-export default (popName: string, sentenceGroup: SentenceGroup) => {
+export default (popName: string, sentenceGroup: SentenceGroup, ftr56SentenceConvictionFeatureFlag: boolean) => {
   if (sentenceGroup === SentenceGroup.ADULT_SDS) {
+    if (ftr56SentenceConvictionFeatureFlag) {
+      return {
+        isServingTerroristOrNationalSecurityOffence: {
+          label: `Is ${popName} serving a sentence for a terrorist or national security offence?`,
+        },
+        isAtRiskOfInvolvedInForeignPowerThreat: {
+          label: `Is ${popName} considered to be a person at risk of being involved in foreign power threat activity?`,
+        },
+        wasReferredToParoleBoard244ZB: {
+          label: `Was ${popName} referred to the Parole Board under section 244ZB (power to detain) on this sentence?`,
+        },
+        wasRepatriatedForMurder: {
+          label: `Has ${popName} been repatriated to the UK following a sentence for murder?`,
+        },
+        isServingSOPCSentence: {
+          label: `Is ${popName} serving a Sentence for offenders of particular concern (SOPC)?`,
+        },
+        isServingDCRSentence: {
+          label: `Is ${popName} serving a Discretionary conditional release (DCR) sentence?`,
+        },
+      }
+    }
     return {
       isChargedWithOffence: {
         label: `Is ${popName} being recalled because of being charged with an offence?`,
