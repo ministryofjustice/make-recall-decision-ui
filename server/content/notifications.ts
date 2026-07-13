69a7dcf89f130983420fc3da18cc566aa2745a0a
Pablo Mascaraque Glenn <pablo.glenn@digital.justice.gov.uk>
2026-07-13 17:27:36 +0100

    MRD-3261 Update FTR56 sentence conviction banner (#1648)

--- server/content/notifications.ts
+++ server/content/notifications.ts
@@ -6,7 +6,7 @@ const notifications: Notification[] = [
     headerText: 'Out-of-hours recalls started on 28 July 2026 will need to be completed by 11:59pm',
     bodyContent:
       'This is because the service needs to be updated in line with new FTR56 rules and changes to the Part A document.',
-    startDate: new Date('2025-07-14T23:00:00Z'), // 00:00 15th July 2026 BST (set to 2025 for testing, will be updated before releasing)
+    startDate: new Date('2026-07-14T23:00:00Z'), // 00:00 15th July 2026 BST
     endDate: new Date('2026-07-28T23:00:00Z'), // 00:00 29th July 2026 BST
     visibleToRoles: [HMPPS_AUTH_ROLE.RW, HMPPS_AUTH_ROLE.ODM],
   },
@@ -14,7 +14,7 @@ const notifications: Notification[] = [
     headerText: 'Any Part A not downloaded by 11.59pm on 28 July 2026 will be deleted',
     bodyContent:
       'This is because the service needs to be updated in line with new FTR56 rules and changes to the Part A document. You can start the recommendation again after the service resumes at 12am on 29 July 2026.',
-    startDate: new Date('2025-07-14T23:00:00Z'), // 00:00 15th July 2026 BST (set to 2025 for testing, will be updated before releasing)
+    startDate: new Date('2026-07-14T23:00:00Z'), // 00:00 15th July 2026 BST
     endDate: new Date('2026-07-28T23:00:00Z'), // 00:00 29th July 2026 BST
     visibleToRoles: [HMPPS_AUTH_ROLE.PO, HMPPS_AUTH_ROLE.SPO],
   },
