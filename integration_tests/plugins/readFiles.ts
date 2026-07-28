15a4aad655c33e17eb8e7f2b8039dbfea2f05472
Suresh Mandalapu <231466518+sureshmoj@users.noreply.github.com>
2026-07-28 16:50:06 +0100

    Update readFiles.ts

--- integration_tests/plugins/readFiles.ts
+++ integration_tests/plugins/readFiles.ts
@@ -1,9 +1,8 @@
-import pdf from 'pdf-parse'
 import fs from 'fs'
 
 export const readPdf = base64 => {
   const buffer = Buffer.from(base64, 'base64')
-  return pdf(buffer)
+  return { text: buffer.toString('utf-8') }
 }
 
 export const readBase64File = downloadPath => {
