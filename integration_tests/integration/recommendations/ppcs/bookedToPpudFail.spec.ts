4d0ccaba56b924c2a0f905f3fcf0aeaec6d5b00b
Bhanu Yandra <bhanu.yandra@justice.gov.uk>
2026-09-10 10:18:44 +0100

    MRD-3230: integration tests

--- integration_tests/integration/recommendations/ppcs/bookedToPpudFail.spec.ts
+++ integration_tests/integration/recommendations/ppcs/bookedToPpudFail.spec.ts
@@ -22,7 +22,7 @@ context('Booked to PPUD failed', () => {
 
     cy.task('getStatuses', {
       statusCode: 200,
-      response: [{ name: RECOMMENDATION_STATUS.BOOKED_TO_PPUD, active: true }],
+      response: [{ name: RECOMMENDATION_STATUS.BOOKING_ON_STARTED, active: true }],
     })
   })
 
