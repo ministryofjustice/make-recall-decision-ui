c5589eb8ffdb9ca494d99e36d0942594e5eb23be
fincardy-moj <fin.cardy@justice.gov.uk>
2026-09-16 15:01:08 +0100

    MRD-3377 Licence conditions notification banner

--- server/content/notifications.ts
+++ server/content/notifications.ts
@@ -1,5 +1,5 @@
 import { Notification } from '../@types/notification'
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+
 import { HMPPS_AUTH_ROLE } from '../middleware/authorisationMiddleware'
 
 /**
@@ -16,6 +16,23 @@ import { HMPPS_AUTH_ROLE } from '../middleware/authorisationMiddleware'
     visibleToRoles: [HMPPS_AUTH_ROLE.RW], // Array of HMPPS_AUTH_ROLEs
   },
  */
-const notifications: Notification[] = []
+const notifications: Notification[] = [
+  {
+    headerText: 'Any Part A not downloaded by 11.59pm on 30 September 2026 will be deleted',
+    bodyContent:
+      'This is because the service needs to be updated in line with licence condition updates and changes to the Part A document. You can start the recommendation again after the service resumes at 12am on 1st October 2026.',
+    startDate: new Date('2026-09-16T00:00:00Z'), // Z-Indexed ISO Date
+    endDate: new Date('2026-09-30T22:59:59Z'), // Z-Indexed ISO Date
+    visibleToRoles: [HMPPS_AUTH_ROLE.SPO, HMPPS_AUTH_ROLE.PO],
+  },
+  {
+    headerText: 'Out-of-hours recalls started on 30 September 2026 will need to be completed by 11:59pm',
+    bodyContent:
+      'This is because the service needs to be updated in line with licence condition updates and changes to the Part A document.',
+    startDate: new Date('2026-09-16T00:00:00Z'), // Z-Indexed ISO Date
+    endDate: new Date('2026-09-30T22:59:59Z'), // Z-Indexed ISO Date
+    visibleToRoles: [HMPPS_AUTH_ROLE.ODM, HMPPS_AUTH_ROLE.RW],
+  },
+]
 
 export default notifications
